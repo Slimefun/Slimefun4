329a191d3c448aa7753d65d5353fd11d720652c2
BuildTools <aggelikiandgiannis@gmail.com>
2016-09-11 22:55:39 +0200

    Now you shouldn't be able to fix a broken spawner with any items.

--- src/me/mrCookieSlime/Slimefun/AncientAltar/Pedestals.java
+++ src/me/mrCookieSlime/Slimefun/AncientAltar/Pedestals.java
@@ -13,12 +13,12 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class Pedestals {
-	
+
 	public static List<AltarRecipe> recipes = new ArrayList<AltarRecipe>();
-	
+
 	public static List<Block> getPedestals(Block altar) {
 		List<Block> list = new ArrayList<Block>();
-		
+
 		if (BlockStorage.check(altar.getRelative(3, 0, 0), "ANCIENT_PEDESTAL")) {
 			list.add(altar.getRelative(3, 0, 0));
 		}
@@ -43,28 +43,33 @@ public class Pedestals {
 		if (BlockStorage.check(altar.getRelative(-2, 0, -2), "ANCIENT_PEDESTAL")) {
 			list.add(altar.getRelative(-2, 0, -2));
 		}
-		
+
 		return list;
 	}
-	
+
 	public static ItemStack getRecipeOutput(ItemStack catalyst, List<ItemStack> input) {
 		if (input.size() != 8) return null;
 		if (SlimefunManager.isItemSimiliar(catalyst, SlimefunItems.BROKEN_SPAWNER, false)) {
+            if (checkRecipe(SlimefunItems.BROKEN_SPAWNER, input) == null) return null;
 			final ItemStack spawner = SlimefunItems.REPAIRED_SPAWNER.clone();
 			ItemMeta im = spawner.getItemMeta();
 			im.setLore(Arrays.asList(catalyst.getItemMeta().getLore().get(0)));
 			spawner.setItemMeta(im);
 			return spawner;
 		}
-		
-		AltarRecipe r = null;
-		for (AltarRecipe recipe: recipes) {
+
+		return checkRecipe(catalyst, input);
+	}
+
+	private static ItemStack checkRecipe(ItemStack catalyst, List<ItemStack> input) {
+        AltarRecipe r = null;
+		for (AltarRecipe recipe : recipes) {
 			if (SlimefunManager.isItemSimiliar(catalyst, recipe.getCatalyst(), true)) {
 				r = recipe;
 				recipe:
-				for (ItemStack item: recipe.getInput()) {
+				for (ItemStack item : recipe.getInput()) {
 					boolean match = false;
-					for (ItemStack item2: input) {
+					for (ItemStack item2 : input) {
 						if (SlimefunManager.isItemSimiliar(item2, item, true)) {
 							match = true;
 						}
@@ -74,12 +79,10 @@ public class Pedestals {
 						break recipe;
 					}
 				}
-			
+
 				if (r != null) return r.getOutput();
 			}
 		}
-		
-		return null;
+        return null;
 	}
-
 }
