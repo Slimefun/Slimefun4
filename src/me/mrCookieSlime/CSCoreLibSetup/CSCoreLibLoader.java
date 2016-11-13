24774ec8780a30cdf7afb96bfd19c8901c8d30a7
TheBusyBiscuit <mrCookieSlime@gmail.com>
2016-11-13 18:13:44 +0100

    Fixed Import

--- src/me/mrCookieSlime/CSCoreLibSetup/CSCoreLibLoader.java
+++ src/me/mrCookieSlime/CSCoreLibSetup/CSCoreLibLoader.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
