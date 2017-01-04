18def9d050ac777127e7c759a10c590168349b74
TheBusyBiscuit <mrCookieSlime@gmail.com>
2017-01-04 17:48:44 +0100

    Machine Settings Pre-Setup

---
+++ src/me/mrCookieSlime/Slimefun/api/machine/MachineConfig.java
@@ -0,0 +1,32 @@
+package me.mrCookieSlime.Slimefun.api.machine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
+
+public class MachineConfig extends Config {
+	
+	MachineSettings global;
+	Map<String, MachineSettings> children;
+	
+	public MachineConfig(String id) {
+		super("plugins/Slimefun/machines/" + id + ".yml");
+		
+		this.global = new MachineSettings(this);
+		this.children = new HashMap<String, MachineSettings>();
+	}
+	
+	public MachineSettings getGlobalSettings() {
+		return this.global;
+	}
+	
+	public MachineSettings getSettings(AContainer item) {
+		if (!this.children.containsKey(item.getName())) {
+			this.children.put(item.getName(), new MachineSettings(this, item));
+		}
+
+		return this.children.get(item.getName());
+	}
+}
