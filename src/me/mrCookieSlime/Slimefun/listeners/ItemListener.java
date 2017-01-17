0bf99d6974670d68e359fd1e653d3011c13c3c27
TheBusyBiscuit <mrCookieSlime@gmail.com>
2017-01-17 16:37:55 +0100

    Fixed double messages

--- src/me/mrCookieSlime/Slimefun/listeners/ItemListener.java
+++ src/me/mrCookieSlime/Slimefun/listeners/ItemListener.java
@@ -89,21 +89,15 @@ public class ItemListener implements Listener {
 						p.sendMessage(" ");
 						p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&d" + e.getClickedBlock().getType() + ":" + e.getClickedBlock().getData() + " &e@ X: " + e.getClickedBlock().getX() + " Y: " + e.getClickedBlock().getY() + " Z: " + e.getClickedBlock().getZ()));
 						p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dID: " + "&e" + BlockStorage.checkID(e.getClickedBlock())));
-						p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&d" + e.getClickedBlock().getType() + ":" + e.getClickedBlock().getData() + " &e@ X: " + e.getClickedBlock().getX() + " Y: " + e.getClickedBlock().getY() + " Z: " + e.getClickedBlock().getZ()));
-						p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dID: " + "&e" + BlockStorage.checkID(e.getClickedBlock())));
 						if (e.getClickedBlock().getState() instanceof Skull) {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dSkull: " + "&2\u2714"));
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dRotation: &e" + ((Skull) e.getClickedBlock().getState()).getRotation().toString()));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dSkull: " + "&2\u2714"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dRotation: &e" + ((Skull) e.getClickedBlock().getState()).getRotation().toString()));
 						}
 						if (BlockStorage.getStorage(e.getClickedBlock().getWorld()).hasInventory(e.getClickedBlock().getLocation())) {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dInventory: " + "&2\u2714"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dInventory: " + "&2\u2714"));
 						}
 						else {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dInventory: " + "&4\u2718"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dInventory: " + "&4\u2718"));
 						}
 						if (BlockStorage.check(e.getClickedBlock()).isTicking()) {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dTicking: " + "&2\u2714"));
@@ -111,19 +105,11 @@ public class ItemListener implements Listener {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dTimings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock()) + "ms"));
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dTotal Timings: &e" + SlimefunStartup.ticker.getTimings(BlockStorage.checkID(e.getClickedBlock())) + "ms"));
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dChunk Timings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock().getChunk()) + "ms"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dTicking: " + "&2\u2714"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dAsync: &e" + (BlockStorage.check(e.getClickedBlock()).getTicker().isSynchronized() ? "&4\u2718": "&2\u2714")));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dTimings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock()) + "ms"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dTotal Timings: &e" + SlimefunStartup.ticker.getTimings(BlockStorage.checkID(e.getClickedBlock())) + "ms"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dChunk Timings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock().getChunk()) + "ms"));
 						}
 						else if (BlockStorage.check(e.getClickedBlock()).getEnergyTicker() != null) {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dTicking: " + "&b~ &3(Indirect)"));
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dTimings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock()) + "ms"));
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dChunk Timings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock().getChunk()) + "ms"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dTicking: " + "&b~ &3(Indirect)"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dTimings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock()) + "ms"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dChunk Timings: &e" + SlimefunStartup.ticker.getTimings(e.getClickedBlock().getChunk()) + "ms"));
 						}
 						else {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dTicking: " + "&4\u2718"));
@@ -132,17 +118,12 @@ public class ItemListener implements Listener {
 						if (ChargableBlock.isChargable(e.getClickedBlock())) {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dChargable: " + "&2\u2714"));
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "  &dEnergy: &e" + ChargableBlock.getCharge(e.getClickedBlock()) + " / " + ChargableBlock.getMaxCharge(e.getClickedBlock())));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dChargable: " + "&2\u2714"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"  &dEnergy: &e" + ChargableBlock.getCharge(e.getClickedBlock()) + " / " + ChargableBlock.getMaxCharge(e.getClickedBlock())));
 						}
 						else {
 							p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dChargable: " + "&4\u2718"));
-							p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&dChargable: " + "&4\u2718"));
 						}
 						p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6" + BlockStorage.getBlockInfoAsJson(e.getClickedBlock())));
 						p.sendMessage(" ");
-						p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6" + BlockStorage.getBlockInfoAsJson(e.getClickedBlock())));
-						p.sendMessage(ChatColor.translateAlternateColorCodes('&'," "));
 					}
 					break;
 				}
