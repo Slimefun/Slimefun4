3bba53b20d3d3bf2630dbcf827d80c166c6f7424
Redemption <25185835+Redemption198@users.noreply.github.com>
2017-11-19 22:59:58 +0100

    Added permission check to /sf guide command
    The /sf guide command will now only be executed if the player has the permission "slimefun.command.guide"

--- src/me/mrCookieSlime/Slimefun/Commands/SlimefunCommand.java
+++ src/me/mrCookieSlime/Slimefun/Commands/SlimefunCommand.java
@@ -105,16 +105,17 @@ public class SlimefunCommand implements CommandExecutor, Listener {
 				}
 				else Messages.local.sendTranslation(sender, "messages.only-players", true);
 			}
-			else if(args[0].equalsIgnoreCase("open_guide")) {
-				if (sender instanceof Player) { 
-					if(sender.hasPermission("slimefun.command.open_guide")) SlimefunGuide.openGuide((Player) sender, SlimefunStartup.getCfg().getBoolean("guide.default-view-book"));
+			else if (args[0].equalsIgnoreCase("guide")) {
+				if (sender instanceof Player) {
+					if(sender.hasPermission("slimefun.command.guide"))((Player) sender).getInventory().addItem(SlimefunGuide.getItem(SlimefunStartup.getCfg().getBoolean("guide.default-view-book")));
 					else Messages.local.sendTranslation(sender, "messages.no-permission", true);
 				}
 				else Messages.local.sendTranslation(sender, "messages.only-players", true);
 			}
-			else if (args[0].equalsIgnoreCase("guide")) {
-				if (sender instanceof Player) {
-					((Player) sender).getInventory().addItem(SlimefunGuide.getItem(SlimefunStartup.getCfg().getBoolean("guide.default-view-book")));
+			else if(args[0].equalsIgnoreCase("open_guide")) {
+				if (sender instanceof Player) { 
+					if(sender.hasPermission("slimefun.command.open_guide")) SlimefunGuide.openGuide((Player) sender, SlimefunStartup.getCfg().getBoolean("guide.default-view-book"));
+					else Messages.local.sendTranslation(sender, "messages.no-permission", true);
 				}
 				else Messages.local.sendTranslation(sender, "messages.only-players", true);
 			}
