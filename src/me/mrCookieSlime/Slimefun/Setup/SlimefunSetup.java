815557c29ec1cb92c7e7aaa39dfbd3cf32c36f44
Redemption <25185835+Redemption198@users.noreply.github.com>
2017-12-28 11:44:54 +0100

    Update SlimefunSetup.java

--- src/me/mrCookieSlime/Slimefun/Setup/SlimefunSetup.java
+++ src/me/mrCookieSlime/Slimefun/Setup/SlimefunSetup.java
@@ -3114,10 +3114,12 @@ public class SlimefunSetup {
 			@Override
 			public boolean onBreak(Player p, Block b, SlimefunItem item, UnregisterReason reason) {
 				Item stack = AncientAltarListener.findItem(b);
-				if (stack != null) stack.removeMetadata("item_placed", SlimefunStartup.instance);
-				if(reason == UnregisterReason.PLAYER_BREAK || reason == UnregisterReason.EXPLODE) {
-					b.getWorld().dropItem(b.getLocation(), AncientAltarListener.fixItemStack(stack.getItemStack(), stack.getCustomName()));
-					stack.remove();
+				if (stack != null) { 
+					stack.removeMetadata("item_placed", SlimefunStartup.instance);
+					if(reason == UnregisterReason.PLAYER_BREAK || reason == UnregisterReason.EXPLODE) {
+						b.getWorld().dropItem(b.getLocation(), AncientAltarListener.fixItemStack(stack.getItemStack(), stack.getCustomName()));
+						stack.remove();
+					}
 				}
 				return true;
 			}
