f5d9170961b89a6815aba34cb32dbaa5240bead8
TheBusyBiscuit <TheBusyBiscuit@users.noreply.github.com>
2017-12-29 12:01:22 +0100

    Fixed Import

--- src/me/mrCookieSlime/Slimefun/api/item_transport/CargoNet.java
+++ src/me/mrCookieSlime/Slimefun/api/item_transport/CargoNet.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
