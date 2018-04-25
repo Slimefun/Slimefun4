76af3df1be843c14ec8b8a9d1afc439d3912cec5
Rick <mrcoffee1026@gmail.com>
2018-04-25 18:16:20 -0400

    Adjustments / simplified

--- src/me/mrCookieSlime/Slimefun/listeners/AncientAltarListener.java
+++ src/me/mrCookieSlime/Slimefun/listeners/AncientAltarListener.java
@@ -51,20 +51,15 @@ public class AncientAltarListener implements Listener {
 		Block b = e.getClickedBlock();
 
 		SlimefunItem item = BlockStorage.check(b);
-		
+
 		if (item != null) {
 			if (item.getID().equals("ANCIENT_PEDESTAL")) {
-				Block verifyblock = findAncientAltar(b);
-				if (verifyblock != null) {
-					if (Variables.altarinuse.contains(verifyblock.getLocation())) {
-						e.setCancelled(true);
-						return;
-					}
-				} else {
+
+				if (Variables.altarinuse.contains(b.getLocation())) {
 					e.setCancelled(true);
 					return;
-				}				
-				
+				}
+
 				e.setCancelled(true);
 				Item stack = findItem(b);
 				if (stack == null) {
@@ -98,12 +93,8 @@ public class AncientAltarListener implements Listener {
 				if (Variables.altarinuse.contains(b.getLocation())) {
 					e.setCancelled(true);
 					return;
-				} else if (!Variables.altarinuse.contains(b.getLocation())) {
-					Variables.altarinuse.add(b.getLocation());  // make altarinuse simply because that was the last block clicked.
-				} else {
-					e.setCancelled(true);
-					return;
 				}
+				Variables.altarinuse.add(b.getLocation());  // make altarinuse simply because that was the last block clicked.
 				e.setCancelled(true);
 
 				ItemStack catalyst = new CustomItem(e.getPlayer().getInventory().getItemInMainHand(), 1);
@@ -112,6 +103,10 @@ public class AncientAltarListener implements Listener {
 				if (!altars.contains(e.getClickedBlock())) {
 					altars.add(e.getClickedBlock());
 					if (pedestals.size() == 8) {
+						pedestals.forEach((pblock)->{
+							Variables.altarinuse.add(pblock.getLocation());
+						});
+
 						if (catalyst != null && !catalyst.getType().equals(Material.AIR)) {
 							List<ItemStack> input = new ArrayList<ItemStack>();
 							for (Block pedestal: pedestals) {
@@ -123,25 +118,36 @@ public class AncientAltarListener implements Listener {
 								List<ItemStack> consumed = new ArrayList<ItemStack>();
 								consumed.add(catalyst);
 								PlayerInventory.consumeItemInHand(e.getPlayer());
-								if (!Variables.altarinuse.contains(b.getLocation())) Variables.altarinuse.add(b.getLocation());  // altarinuse already, but in case spam clicking meant it wasn't...
 								Bukkit.getScheduler().scheduleSyncDelayedTask(SlimefunStartup.instance, new RitualAnimation(altars, b, b.getLocation().add(0.5, 1.3, 0.5), result, pedestals, consumed), 10L);
 							}
 							else {
 								altars.remove(e.getClickedBlock());
 								Messages.local.sendTranslation(e.getPlayer(), "machines.ANCIENT_ALTAR.unknown-recipe", true);
-								if (Variables.altarinuse.contains(b.getLocation())) Variables.altarinuse.remove(b.getLocation());  // bad recipe, no longer in use.
+
+								pedestals.forEach((pblock)->{
+									Variables.altarinuse.remove(pblock.getLocation());
+								});
+								Variables.altarinuse.remove(b.getLocation());  // bad recipe, no longer in use.
 							}
 						}
 						else {
 							altars.remove(e.getClickedBlock());
 							Messages.local.sendTranslation(e.getPlayer(), "machines.ANCIENT_ALTAR.unknown-catalyst", true);
-							if (Variables.altarinuse.contains(b.getLocation())) Variables.altarinuse.remove(b.getLocation());  // unkown catalyst, no longer in use
+
+							pedestals.forEach((pblock)->{
+								Variables.altarinuse.remove(pblock.getLocation());
+							});
+							Variables.altarinuse.remove(b.getLocation());  // unkown catalyst, no longer in use
 						}
 					}
 					else {
 						altars.remove(e.getClickedBlock());
 						Messages.local.sendTranslation(e.getPlayer(), "machines.ANCIENT_ALTAR.not-enough-pedestals", true, new Variable("%pedestals%", String.valueOf(pedestals.size())));
-						if (Variables.altarinuse.contains(b.getLocation())) Variables.altarinuse.remove(b.getLocation());  // not a proper altar, so not in use.
+
+						pedestals.forEach((pblock)->{
+							Variables.altarinuse.remove(pblock.getLocation());
+						});
+						Variables.altarinuse.remove(b.getLocation());  // not a proper altar, so not in use.
 					}
 				}
 			}
@@ -170,47 +176,6 @@ public class AncientAltarListener implements Listener {
 			}
 		}
 		return null;
-	}
-
-	private static Block getRelAltarBlock(Block b, Integer xC, Integer zC) {
-		Block rel = b.getRelative(xC, 0, zC);
-		SlimefunItem item = BlockStorage.check(rel);
-		if (item != null) {
-			if (item.getID().equals("ANCIENT_ALTAR")) return rel;
-		}
-		Block nonefound = null;
-		return nonefound;  // if here then relative coordinates did not find an Altar
-	}
-
-	public static Block findAncientAltar(Block b) {
-		SlimefunItem item = BlockStorage.check(b);
-		if (item != null) {
-
-			if (item.getID().equals("ANCIENT_ALTAR")) return b;
-			if ((item.getID().equals("ANCIENT_PEDESTAL"))) {
-
-				List<Block> foundaltars = new ArrayList<Block>();
-
-				// Altar linear positions
-				if (getRelAltarBlock(b,3,0) != null) foundaltars.add(getRelAltarBlock(b,3,0));
-				if (getRelAltarBlock(b,0,3) != null) foundaltars.add(getRelAltarBlock(b,0,3));
-				if (getRelAltarBlock(b,-3,0) != null) foundaltars.add(getRelAltarBlock(b,-3,0));
-				if (getRelAltarBlock(b,0,-3) != null) foundaltars.add(getRelAltarBlock(b,0,-3));
-
-				// Altar Diagonal Positions:
-				if (getRelAltarBlock(b,-2,2) != null) foundaltars.add(getRelAltarBlock(b,-2,2));
-				if (getRelAltarBlock(b,-2,-2) != null) foundaltars.add(getRelAltarBlock(b,-2,-2));
-				if (getRelAltarBlock(b,2,2) != null) foundaltars.add(getRelAltarBlock(b,2,2));
-				if (getRelAltarBlock(b,2,-2) != null) foundaltars.add(getRelAltarBlock(b,2,-2));
-
-				Block fakealtarsetupfound = null;
-				if (foundaltars.size() > 1) return fakealtarsetupfound;  // effectively disable bypass by placing multiple altar blocks around altar.
-				if (foundaltars.size() == 1) return foundaltars.get(0);
-			}
-		}
-
-		Block noblockfound = null;
-		return noblockfound;  // should never get here.
 	}
 
 	private void insertItem(Player p, Block b) {
