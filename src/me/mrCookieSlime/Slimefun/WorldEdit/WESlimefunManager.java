af59dfa5eacccb61f451c918429776516d6aefb9
PrzemoVi <przemowi2002@gmail.com>
2018-12-06 22:17:26 +0100

    Add files via upload
    WorldEdit 7.0 fix

--- src/me/mrCookieSlime/Slimefun/WorldEdit/WESlimefunManager.java
+++ src/me/mrCookieSlime/Slimefun/WorldEdit/WESlimefunManager.java
@@ -1,17 +1,17 @@
 package me.mrCookieSlime.Slimefun.WorldEdit;
 
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 
 import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.WorldEdit;
-import com.sk89q.worldedit.blocks.BaseBlock;
+import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.event.extent.EditSessionEvent;
-import com.sk89q.worldedit.extent.logging.AbstractLoggingExtent;
+import com.sk89q.worldedit.extent.AbstractDelegateExtent;
 import com.sk89q.worldedit.util.eventbus.Subscribe;
+import com.sk89q.worldedit.world.block.BlockStateHolder;
 
 public class WESlimefunManager {
 	
@@ -21,13 +21,11 @@ public class WESlimefunManager {
 	
 	@Subscribe
     public void wrapForLogging(final EditSessionEvent event) {
-		event.setExtent(new AbstractLoggingExtent(event.getExtent()) {
-			
-			@Override
-			protected void onBlockChange(Vector pos, BaseBlock b) {
-				super.onBlockChange(pos, b);
-				
-				if (b.getType() == 0) {
+		
+		event.setExtent(new AbstractDelegateExtent(event.getExtent()) {
+
+			public boolean setBlock(Vector pos, BlockStateHolder block) throws WorldEditException {
+				if (block.getBlockType().getLegacyId() == 0) {
 					World world = Bukkit.getWorld(event.getWorld().getName());
 					
 					if (world != null) {
@@ -35,7 +33,8 @@ public class WESlimefunManager {
 						if (BlockStorage.hasBlockInfo(l)) BlockStorage.clearBlockInfo(l);
 					}
 				}
-			}
+                return getExtent().setBlock(pos, block);
+            }
 			
 		});
     }
