a622e05ea88080705c668d41fdbaad42d4def1a3
sarhatabaot <sarhatabaot1@gmail.com>
2019-05-04 11:06:01 +0300

    Refactored Talisman
    Refactored the class for better readability.
    * Added getPlayerByEventType method, instead of calling it directly from checkFor().
    * Added pass method.
    * Checking for false results first to reduce the amount of if-else.
    * Renamed SlimefunItem talisman -> SlimefunItem item.
    * Created new Talisman talisman = (Talisman) item, after instanceof check.
    * Note: Casting the item every time to talisman clutters the code.

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/Talisman.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/Talisman.java
@@ -28,129 +28,150 @@ import org.bukkit.potion.PotionEffect;
  */
 public class Talisman extends SlimefunItem {
 
-	private String suffix;
-	private boolean consumable = true;
-	private boolean cancel = true;
-	private PotionEffect[] effects = new PotionEffect[0];
-	private int chance = 100;
+    private String suffix;
+    private boolean consumable = true;
+    private boolean cancel = true;
+    private PotionEffect[] effects;
+    private int chance = 100;
 
-	public Talisman(ItemStack item, String id, ItemStack[] recipe, boolean consumable, boolean cancelEvent, String messageSuffix, PotionEffect... effects) {
-		super(Categories.TALISMANS_1, item, id, RecipeType.MAGIC_WORKBENCH, recipe, new CustomItem(item, consumable ? 4: 1));
-		this.consumable = consumable;
-		this.cancel = cancelEvent;
-		this.suffix = messageSuffix;
-		this.effects = effects;
-	}
-	
-	public Talisman(ItemStack item, String id, ItemStack[] recipe, boolean consumable, boolean cancelEvent, String messageSuffix, int chance, PotionEffect... effects) {
-		super(Categories.TALISMANS_1, item, id, RecipeType.MAGIC_WORKBENCH, recipe, new CustomItem(item, consumable ? 4: 1));
-		this.consumable = consumable;
-		this.cancel = cancelEvent;
-		this.suffix = messageSuffix;
-		this.effects = effects;
-		this.chance = chance;
-	}
-	
-	public Talisman(ItemStack item, String id, ItemStack[] recipe, String messageSuffix, int chance, PotionEffect... effects) {
-		super(Categories.TALISMANS_1, item, id, RecipeType.MAGIC_WORKBENCH, recipe, item);
-		this.suffix = messageSuffix;
-		this.effects = effects;
-		this.chance = chance;
-	}
+    public Talisman(ItemStack item, String id, ItemStack[] recipe, boolean consumable, boolean cancelEvent, String messageSuffix, PotionEffect... effects) {
+        super(Categories.TALISMANS_1, item, id, RecipeType.MAGIC_WORKBENCH, recipe, new CustomItem(item, consumable ? 4 : 1));
+        this.consumable = consumable;
+        this.cancel = cancelEvent;
+        this.suffix = messageSuffix;
+        this.effects = effects;
+    }
 
-	public String getSuffix() 			{		return this.suffix;		}
-	public boolean isConsumable()	 	{		return this.consumable;	}
-	public boolean isEventCancelled() 	{		return this.cancel;		}
-	public PotionEffect[] getEffects()	{		return this.effects;	}
-	public int getChance()				{		return this.chance;		}
+    public Talisman(ItemStack item, String id, ItemStack[] recipe, boolean consumable, boolean cancelEvent, String messageSuffix, int chance, PotionEffect... effects) {
+        super(Categories.TALISMANS_1, item, id, RecipeType.MAGIC_WORKBENCH, recipe, new CustomItem(item, consumable ? 4 : 1));
+        this.consumable = consumable;
+        this.cancel = cancelEvent;
+        this.suffix = messageSuffix;
+        this.effects = effects;
+        this.chance = chance;
+    }
 
-	public static boolean checkFor(Event e, SlimefunItem talisman) {
-		if (talisman != null) {
-			if (talisman instanceof Talisman) {
-				boolean message = !((Talisman) talisman).getSuffix().equalsIgnoreCase("");
-				if (SlimefunStartup.chance(100, ((Talisman) talisman).getChance())) {
-					Player p = null;
-					
-					if (e instanceof EntityDeathEvent) p = ((EntityDeathEvent) e).getEntity().getKiller();
-					else if (e instanceof BlockBreakEvent) p = ((BlockBreakEvent) e).getPlayer();
-					else if (e instanceof PlayerEvent) p = ((PlayerEvent) e).getPlayer();
-					else if (e instanceof EntityEvent) p = (Player) ((EntityEvent) e).getEntity();
-					else if (e instanceof EnchantItemEvent) p = ((EnchantItemEvent) e).getEnchanter();
-					
-					boolean pass = true;
-					
-					for (PotionEffect effect: ((Talisman) talisman).getEffects()) {
-						if (effect != null && p.hasPotionEffect(effect.getType())) pass = false;
-					}
-					
-					if (pass) {
-						if (p.getInventory().containsAtLeast(talisman.getItem(), 1)) {
-							if (Slimefun.hasUnlocked(p, talisman.getItem(), true)) {
-								if (((Talisman) talisman).isConsumable()) p.getInventory().removeItem(talisman.getItem());
-								for (PotionEffect effect: ((Talisman) talisman).getEffects()) {
-									p.addPotionEffect(effect);
-								}
-								
-								if (e instanceof Cancellable && ((Talisman) talisman).isEventCancelled()) ((Cancellable) e).setCancelled(true);
-								
-								if (message) Messages.local.sendTranslation(p, "messages.talisman." + ((Talisman) talisman).getSuffix(),true);
-								
-								return true;
-							}
-							else return false;
-						}
-						else if (p.getEnderChest().containsAtLeast(((Talisman) talisman).upgrade(), 1)) {
-							if (Slimefun.hasUnlocked(p, ((Talisman) talisman).upgrade(), true)) {
-								if (((Talisman) talisman).isConsumable()) p.getEnderChest().removeItem(((Talisman) talisman).upgrade());
-								for (PotionEffect effect: ((Talisman) talisman).getEffects()) {
-									p.addPotionEffect(effect);
-								}
-								if (e instanceof Cancellable && ((Talisman) talisman).isEventCancelled()) ((Cancellable) e).setCancelled(true);
-								
-								if (message) Messages.local.sendTranslation(p, "messages.talisman." + ((Talisman) talisman).getSuffix(), true);
-								
-								return true;
-							}
-							else return false;
-						}
-						else return false;
-					}
-					else return false;
-				}
-				else return false;
-			}
-			else return false;
-		}
-		else return false;
-	}
+    public Talisman(ItemStack item, String id, ItemStack[] recipe, String messageSuffix, int chance, PotionEffect... effects) {
+        super(Categories.TALISMANS_1, item, id, RecipeType.MAGIC_WORKBENCH, recipe, item);
+        this.suffix = messageSuffix;
+        this.effects = effects;
+        this.chance = chance;
+    }
 
-	public ItemStack upgrade() {
-		List<String> lore = new ArrayList<String>();
-		lore.add("&7&oEnder Infused");
-		lore.add("");
-		for (String line: getItem().getItemMeta().getLore()) {
-			lore.add(line);
-		}
-		ItemStack item = new CustomItem(getItem().getType(), "&5Ender " + ChatColor.stripColor(getItem().getItemMeta().getDisplayName()), lore.toArray(new String[lore.size()]));
-		return item;
-	}
-	
-	@Override
-	public void create() {
-		EnderTalisman talisman = new EnderTalisman(this);
-		talisman.register(true);
-	}
-	
-	@Override
-	public void install() {
-		EnderTalisman talisman = (EnderTalisman) SlimefunItem.getByItem(upgrade());
-		Research research = Research.getByID(112);
-		if (talisman != null) {
-			Slimefun.addOfficialWikiPage(talisman.getID(), "Talismans");
-			if (research != null) talisman.bindToResearch(research);
-		}
-		
-		Slimefun.addOfficialWikiPage(getID(), "Talismans");
-	}
+    public String getSuffix() {
+        return this.suffix;
+    }
+
+    public boolean isConsumable() {
+        return this.consumable;
+    }
+
+    public boolean isEventCancelled() {
+        return this.cancel;
+    }
+
+    public PotionEffect[] getEffects() {
+        return this.effects;
+    }
+
+    public int getChance() {
+        return this.chance;
+    }
+
+    public static boolean checkFor(Event e, SlimefunItem item) {
+        if (!(item instanceof Talisman)) {
+            return false;
+        }
+
+        Talisman talisman = (Talisman) item;
+        if (!SlimefunStartup.chance(100, talisman.getChance())) {
+            return false;
+        }
+
+        Player p = getPlayerByEventType(e);
+        if (!pass(p, talisman)) {
+            return false;
+        }
+        boolean message = !talisman.getSuffix().equalsIgnoreCase("");
+
+        if (p.getInventory().containsAtLeast(talisman.getItem(), 1)) {
+            if (Slimefun.hasUnlocked(p, talisman.getItem(), true)) {
+                if (talisman.isConsumable())
+                    p.getInventory().removeItem(talisman.getItem());
+                for (PotionEffect effect : talisman.getEffects()) {
+                    p.addPotionEffect(effect);
+                }
+
+                if (e instanceof Cancellable && talisman.isEventCancelled())
+                    ((Cancellable) e).setCancelled(true);
+
+                if (message)
+                    Messages.local.sendTranslation(p, "messages.talisman." + talisman.getSuffix(), true);
+
+                return true;
+            } else return false;
+        } else if (p.getEnderChest().containsAtLeast(talisman.upgrade(), 1)) {
+            if (Slimefun.hasUnlocked(p, talisman.upgrade(), true)) {
+                if (talisman.isConsumable())
+                    p.getEnderChest().removeItem(talisman.upgrade());
+                for (PotionEffect effect : talisman.getEffects()) {
+                    p.addPotionEffect(effect);
+                }
+                if (e instanceof Cancellable && talisman.isEventCancelled())
+                    ((Cancellable) e).setCancelled(true);
+
+                if (message)
+                    Messages.local.sendTranslation(p, "messages.talisman." + talisman.getSuffix(), true);
+
+                return true;
+            } else return false;
+        } else return false;
+
+
+    }
+
+    public ItemStack upgrade() {
+        List<String> lore = new ArrayList<>();
+        lore.add("&7&oEnder Infused");
+        lore.add("");
+        for (String line : getItem().getItemMeta().getLore()) {
+            lore.add(line);
+        }
+        return new CustomItem(getItem().getType(), "&5Ender " + ChatColor.stripColor(getItem().getItemMeta().getDisplayName()), lore.toArray(new String[lore.size()]));
+    }
+
+    @Override
+    public void create() {
+        EnderTalisman talisman = new EnderTalisman(this);
+        talisman.register(true);
+    }
+
+    @Override
+    public void install() {
+        EnderTalisman talisman = (EnderTalisman) SlimefunItem.getByItem(upgrade());
+        Research research = Research.getByID(112);
+        if (talisman != null) {
+            Slimefun.addOfficialWikiPage(talisman.getID(), "Talismans");
+            if (research != null) talisman.bindToResearch(research);
+        }
+
+        Slimefun.addOfficialWikiPage(getID(), "Talismans");
+    }
+
+    private static Player getPlayerByEventType(Event e) {
+        if (e instanceof EntityDeathEvent) return ((EntityDeathEvent) e).getEntity().getKiller();
+        else if (e instanceof BlockBreakEvent) return ((BlockBreakEvent) e).getPlayer();
+        else if (e instanceof PlayerEvent) return ((PlayerEvent) e).getPlayer();
+        else if (e instanceof EntityEvent) return (Player) ((EntityEvent) e).getEntity();
+        else if (e instanceof EnchantItemEvent) return ((EnchantItemEvent) e).getEnchanter();
+        return null;
+    }
+
+    private static boolean pass(Player p, SlimefunItem talisman) {
+        for (PotionEffect effect : ((Talisman) talisman).getEffects()) {
+            if (effect != null && p.hasPotionEffect(effect.getType())) return false;
+        }
+        return true;
+    }
 
 }
