1dd32730746b39858f13e1d49077a6d7f52485dc
Daniel Appleby <cgfreethemice@gmail.com>
2019-05-14 20:06:45 -0500

    sybtax correction

--- src/me/mrCookieSlime/Slimefun/api/MySQL/MySQLMain.java
+++ src/me/mrCookieSlime/Slimefun/api/MySQL/MySQLMain.java
@@ -27,12 +27,12 @@ public class MySQLMain {
     {
         instance = this;
         titanSQL = TitanSQL.instance;
-        if (!SlimefunStartup.getCfg().contains("options.mySQL-Enabled"))
+        if (!SlimefunStartup.getCfg().contains("options.mysql-enabled"))
         {
-            SlimefunStartup.getCfg().setValue("options.mySQL-Enabled", false);
+            SlimefunStartup.getCfg().setValue("options.mysql-enabled", false);
             SlimefunStartup.getCfg().save();
         }
-        this.enabled = SlimefunStartup.getCfg().getBoolean("options.mySQL-Enabled");
+        this.enabled = SlimefunStartup.getCfg().getBoolean("options.mysql-enabled");
         if (!this.enabled) return;
         setupTables();
         load_storage = new HashMap<String, List<HashMap<String, ResultData>>>();
