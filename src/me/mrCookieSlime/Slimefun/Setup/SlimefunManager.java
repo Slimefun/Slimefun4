4946d428adc87347cc6597035017730c6e5ea408
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-06-19 08:46:43 +0200

    Fixed Item Recognition System

--- src/me/mrCookieSlime/Slimefun/Setup/SlimefunManager.java
+++ src/me/mrCookieSlime/Slimefun/Setup/SlimefunManager.java
@@ -86,10 +86,6 @@ public class SlimefunManager {
 		if (SFitem == null) return false;
 		
 		if (item.getType() == SFitem.getType() && item.getAmount() >= SFitem.getAmount()) {
-			if (((Damageable) item.getItemMeta()).getDamage() != ((Damageable) SFitem.getItemMeta()).getDamage()) {
-				return false;
-			}
-			
 			if (item.hasItemMeta() && SFitem.hasItemMeta()) {
 				if (item.getItemMeta().hasDisplayName() && SFitem.getItemMeta().hasDisplayName()) {
 					if (item.getItemMeta().getDisplayName().equals(SFitem.getItemMeta().getDisplayName())) {
