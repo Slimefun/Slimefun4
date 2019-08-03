13e0e02dc11e24eb9871ed302ed506a5340662ab
CyberPatriot <CyberPatriot@cyberpatriot.me>
2019-08-03 16:24:34 -0500

    Implement fix for ChestTerminals
    Recently,  there was a duplication glitch with Chest Terminals with androids that I had posted. This fix fixes that glitch and prevents androids from mining player heads altogether.

--- src/me/mrCookieSlime/Slimefun/Android/ProgrammableAndroid.java
+++ src/me/mrCookieSlime/Slimefun/Android/ProgrammableAndroid.java
@@ -83,6 +83,8 @@ public abstract class ProgrammableAndroid extends SlimefunItem {
 		blockblacklist.add(Material.CHAIN_COMMAND_BLOCK);
 		blockblacklist.add(Material.REPEATING_COMMAND_BLOCK);
 		blockblacklist.add(Material.STRUCTURE_BLOCK);
+		blockblacklist.add(Material.PLAYER_HEAD);
+		blockblacklist.add(Material.PLAYER_WALL_HEAD);
 	}
 
 	private Set<MachineFuel> recipes = new HashSet<>();
