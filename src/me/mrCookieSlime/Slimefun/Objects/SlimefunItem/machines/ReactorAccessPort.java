24187648063e3f7f12146cc88e85cc9fb838fabb
dNiym <sales@whoolie.com>
2019-08-18 23:43:22 -0400

    Use a variable to compare reactor id types.

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/machines/ReactorAccessPort.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/machines/ReactorAccessPort.java
@@ -204,7 +204,9 @@ public class ReactorAccessPort extends SlimefunItem {
 	public BlockMenu getReactorMenu(Location l) {
 		Location reactorL = new Location(l.getWorld(), l.getX(), l.getY() - 3, l.getZ());
 		
-		if(BlockStorage.checkID(reactorL).equals("NUCLEAR_REACTOR") || BlockStorage.checkID(reactorL).equals("NETHERSTAR_REACTOR"))
+		String id = BlockStorage.checkID(reactorL);
+		
+		if(id.equals("NUCLEAR_REACTOR") || id.equals("NETHERSTAR_REACTOR"))
 			return BlockStorage.getInventory(reactorL);
 		
 		return null;
