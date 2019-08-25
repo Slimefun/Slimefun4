1859aef846bfa46ff5e76af50816bd5d101e4312
dNiym <sales@whoolie.com>
2019-08-25 17:24:51 -0400

    Fix electric furnaces not being able to use logs.

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/machines/ElectricFurnace.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/machines/ElectricFurnace.java
@@ -3,6 +3,8 @@ package me.mrCookieSlime.Slimefun.Objects.SlimefunItem.machines;
 import java.util.Iterator;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
@@ -26,6 +28,10 @@ public abstract class ElectricFurnace extends AContainer {
 				registerRecipe(4, new ItemStack[] {((FurnaceRecipe) r).getInput()}, new ItemStack[] {r.getResult()});
 			}
 		}
+		
+		//Bukkit Recipe Iterator does not seem to include _LOG's of any type for charcoal... Manually adding them all.
+		for(Material mat:Tag.LOGS.getValues())
+			registerRecipe(4, new ItemStack[] {new ItemStack(mat,1)}, new ItemStack[] {new ItemStack(Material.CHARCOAL, 1)});
 	}
 	
 	@Override
