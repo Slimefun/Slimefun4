6473988df4e27bd61028d4a7c3acd9b23a325a99
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-09-02 11:27:32 +0200

    Fixed Durability Enchantment on damageable Items

--- src/me/mrCookieSlime/Slimefun/utils/DamageableItem.java
+++ src/me/mrCookieSlime/Slimefun/utils/DamageableItem.java
@@ -13,7 +13,11 @@ public interface DamageableItem {
 	boolean isDamageable();
 	
 	default void damageItem(Player p, ItemStack item) {
-		if (item != null && item.getType() != null && item.getType() != Material.AIR && item.getAmount() > 0 && isDamageable() && item.getEnchantments().containsKey(Enchantment.DURABILITY) && Math.random() * 100 <= (60 + Math.floorDiv(40, (item.getEnchantmentLevel(Enchantment.DURABILITY) + 1)))) {
+		if (item != null && item.getType() != null && item.getType() != Material.AIR && item.getAmount() > 0 && isDamageable()) {
+			if (item.getEnchantments().containsKey(Enchantment.DURABILITY) && Math.random() * 100 <= (60 + Math.floorDiv(40, (item.getEnchantmentLevel(Enchantment.DURABILITY) + 1)))) {
+				return;
+			}
+			
 			ItemMeta meta = item.getItemMeta();
 			Damageable damageable = (Damageable) meta;
 			
