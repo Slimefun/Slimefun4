4f38890dc3cbfe1bed0d62fa02727dd922959dc9
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-09-26 15:25:42 +0200

    Added GEO Miner Research

--- src/me/mrCookieSlime/Slimefun/Setup/ResearchSetup.java
+++ src/me/mrCookieSlime/Slimefun/Setup/ResearchSetup.java
@@ -244,5 +244,6 @@ public final class ResearchSetup {
 	    Slimefun.registerResearch(new Research(244, "Diet Cookie", 3), SlimefunItems.DIET_COOKIE);
 	    Slimefun.registerResearch(new Research(245, "Storm Staff", 30), SlimefunItems.STAFF_STORM);
 	    Slimefun.registerResearch(new Research(246, "Soulbound Rune", 60), SlimefunItems.RUNE_SOULBOUND);
+	    Slimefun.registerResearch(new Research(247, "GEO-Miner", 24), SlimefunItems.GEO_MINER);
 	}
 }
