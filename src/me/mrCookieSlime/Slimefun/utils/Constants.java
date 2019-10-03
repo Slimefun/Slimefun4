db0ed1a46b2bf38f59e850d2064967cdfaa1a368
Daniel Walsh <walshydev@gmail.com>
2019-10-04 00:31:47 +0100

    I want a cookie!

--- src/me/mrCookieSlime/Slimefun/utils/Constants.java
+++ src/me/mrCookieSlime/Slimefun/utils/Constants.java
@@ -3,7 +3,11 @@ package me.mrCookieSlime.Slimefun.utils;
 import me.mrCookieSlime.Slimefun.SlimefunPlugin;
 import org.bukkit.NamespacedKey;
 
-public class Constants {
+public final class Constants {
+
+    // Note the naming convention for Namespaces is snake_case
 
     public static final NamespacedKey SF_ITEM = new NamespacedKey(SlimefunPlugin.instance, "sf_item");
+
+    private Constants() {}
 }
