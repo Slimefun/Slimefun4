31f82b899fa07b8b15e9ff1088fe72c87cd57d2f
Daniel Walsh <walshydev@gmail.com>
2019-10-04 00:45:51 +0100

    move id to SlimefunItemStack

--- src/me/mrCookieSlime/Slimefun/api/SlimefunItemStack.java
+++ src/me/mrCookieSlime/Slimefun/api/SlimefunItemStack.java
@@ -3,6 +3,8 @@ package me.mrCookieSlime.Slimefun.api;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 
+import io.github.thebusybiscuit.cscorelib2.data.PersistentDataAPI;
+import me.mrCookieSlime.Slimefun.utils.Constants;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -17,45 +19,50 @@ public class SlimefunItemStack extends CustomItem {
 
 	public SlimefunItemStack(String id, Material type, String name, String... lore) {
 		super(type, name, lore);
-		
-		this.id = id;
+
+		setId(id);
 	}
 
 	public SlimefunItemStack(String id, Material type, Color color, String name, String... lore) {
 		super(new ItemStack(type), color, name, lore);
-		
-		this.id = id;
+
+		setId(id);
 	}
 
 	public SlimefunItemStack(String id, ItemStack item, String name, String... lore) {
 		super(item, name, lore);
-		
-		this.id = id;
+
+		setId(id);
 	}
 
 	public SlimefunItemStack(String id, ItemStack item) {
 		super(item);
-		
-		this.id = id;
+
+		setId(id);
 	}
 
 	public SlimefunItemStack(String id, ItemStack item, Consumer<ItemMeta> consumer) {
 		super(item, consumer);
-		
-		this.id = id;
+
+		setId(id);
 	}
 
 	public SlimefunItemStack(String id, String texture, String name, String... lore) {
 		super(getSkull(texture), name, lore);
 		
+		setId(id);
+	}
+
+	private void setId(String id) {
 		this.id = id;
+		PersistentDataAPI.setString(getItemMeta(), Constants.SF_ITEM, id);
 	}
 	
 	private static ItemStack getSkull(String texture) {
 		try {
 			return CustomSkull.getItem(texture);
 		} catch (Exception x) {
-			Slimefun.getLogger().log(Level.SEVERE, "An Error occured while initializing the Items for Slimefun " + Slimefun.getVersion(), x);
+			Slimefun.getLogger().log(Level.SEVERE, "An Error occurred while initializing the Items for Slimefun " + Slimefun.getVersion(), x);
 			
 			return new ItemStack(Material.PLAYER_HEAD);
 		}
