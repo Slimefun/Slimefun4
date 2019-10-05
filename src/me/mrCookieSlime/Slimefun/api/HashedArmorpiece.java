6797620c2ac151b2ccf55b5ef20310ed2c72f20c
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-10-05 21:35:48 +0200

    Hopefully fixed a minor flaw

--- src/me/mrCookieSlime/Slimefun/api/HashedArmorpiece.java
+++ src/me/mrCookieSlime/Slimefun/api/HashedArmorpiece.java
@@ -3,6 +3,8 @@ package me.mrCookieSlime.Slimefun.api;
 import java.util.Optional;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunArmorPiece;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
@@ -28,7 +30,16 @@ public final class HashedArmorpiece {
 	}
 	
 	public boolean hasDiverged(ItemStack stack) {
-		return stack == null ? hash != 0: stack.hashCode() != hash;
+		if (stack == null) {
+			return hash == 0;
+		}
+		else {
+			ItemStack copy = stack.clone();
+			ItemMeta meta = copy.getItemMeta();
+			((Damageable) meta).setDamage(0);
+			copy.setItemMeta(meta);
+			return copy.hashCode() != hash;
+		}
 	}
 	
 	public Optional<SlimefunArmorPiece> getItem() {
