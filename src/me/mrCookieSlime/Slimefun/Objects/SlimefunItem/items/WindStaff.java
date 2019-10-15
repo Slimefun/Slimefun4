90f3141aecfc6ed000810afedc0ac791aa8b6ace
J3fftw1 <44972470+J3fftw1@users.noreply.github.com>
2019-10-16 00:51:31 +0200

    Update WindStaff.java

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/items/WindStaff.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/items/WindStaff.java
@@ -21,10 +21,7 @@ public class WindStaff extends SimpleSlimefunItem<ItemInteractionHandler> {
 	public WindStaff(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
 		super(category, item, recipeType, recipe);
 	}
-	@Override
-	protected boolean areItemHandlersPrivate() {
-		return false;
-	}
+
 	@Override
 	public ItemInteractionHandler getItemHandler() {
 		return (e, p, item) -> {
