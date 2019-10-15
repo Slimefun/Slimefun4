743a75bd5c20d3cf59a5c6f3e555a5f730387ceb
J3fftw1 <44972470+J3fftw1@users.noreply.github.com>
2019-10-16 00:46:17 +0200

    Update EnderBackpack.java

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/items/EnderBackpack.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/items/EnderBackpack.java
@@ -17,10 +17,6 @@ public class EnderBackpack extends SimpleSlimefunItem<ItemInteractionHandler> {
 		super(category, item, recipeType, recipe);
 	}
 	@Override
-	protected boolean areItemHandlersPrivate() {
-		return false;
-	}
-	@Override
 	public ItemInteractionHandler getItemHandler() {
 		return (e, p, item) -> {
 			if (SlimefunManager.isItemSimiliar(item, SlimefunItems.ENDER_BACKPACK, true)) {
