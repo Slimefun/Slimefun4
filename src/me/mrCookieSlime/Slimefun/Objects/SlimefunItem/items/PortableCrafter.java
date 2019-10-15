c31db3fa532a2b81ae976d9d8829964673be5904
J3fftw1 <44972470+J3fftw1@users.noreply.github.com>
2019-10-16 00:49:58 +0200

    Update PortableCrafter.java

--- src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/items/PortableCrafter.java
+++ src/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/items/PortableCrafter.java
@@ -16,10 +16,7 @@ public class PortableCrafter extends SimpleSlimefunItem<ItemInteractionHandler> 
 	public PortableCrafter(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
 		super(category, item, recipeType, recipe);
 	}
-	@Override
-	protected boolean areItemHandlersPrivate() {
-		return false;
-	}
+
 	@Override
 	public ItemInteractionHandler getItemHandler() {
 		return (e, p, item) -> {
