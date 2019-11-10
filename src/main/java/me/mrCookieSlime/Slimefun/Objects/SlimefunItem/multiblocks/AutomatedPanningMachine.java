7890edd29594c46fb31cfe06e5dd659da41cc13b
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-11-10 19:44:53 +0100

    Added Output Chest Support to the Automated Panning Machine

--- src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/multiblocks/AutomatedPanningMachine.java
+++ src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/multiblocks/AutomatedPanningMachine.java
@@ -2,7 +2,6 @@ package me.mrCookieSlime.Slimefun.Objects.SlimefunItem.multiblocks;
 
 import java.util.Random;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -10,12 +9,15 @@ import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import io.github.thebusybiscuit.cscorelib2.inventory.ItemUtils;
+import io.github.thebusybiscuit.cscorelib2.scheduling.TaskQueue;
 import me.mrCookieSlime.Slimefun.SlimefunPlugin;
 import me.mrCookieSlime.Slimefun.Lists.Categories;
 import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
+import me.mrCookieSlime.Slimefun.Setup.SlimefunManager;
 import me.mrCookieSlime.Slimefun.api.Slimefun;
 
 public class AutomatedPanningMachine extends MultiBlockMachine {
@@ -45,44 +47,64 @@ public class AutomatedPanningMachine extends MultiBlockMachine {
 	public void postRegister() {
 		super.postRegister();
 		
-		chanceSiftedOre = (int) Slimefun.getItemValue("GOLD_PAN", "chance.SIFTED_ORE");
-		chanceClay = (int) Slimefun.getItemValue("GOLD_PAN", "chance.CLAY");
-		chanceFlint = (int) Slimefun.getItemValue("GOLD_PAN", "chance.FLINT");
+		String goldPan = "GOLD_PAN";
+		
+		chanceSiftedOre = (int) Slimefun.getItemValue(goldPan, "chance.SIFTED_ORE");
+		chanceClay = (int) Slimefun.getItemValue(goldPan, "chance.CLAY");
+		chanceFlint = (int) Slimefun.getItemValue(goldPan, "chance.FLINT");
 	}
 	
 	@Override
 	public void onInteract(Player p, Block b) {
 		final ItemStack input = p.getInventory().getItemInMainHand();
-		ItemStack output = null;
 		
-		if (random.nextInt(100) < chanceSiftedOre) output = SlimefunItems.SIFTED_ORE;
-		else if (random.nextInt(100) < chanceClay) output = new ItemStack(Material.CLAY_BALL);
-		else if (random.nextInt(100) < chanceFlint) output = new ItemStack(Material.FLINT);
-		
-		final ItemStack drop = output;
-		
-		if (input != null && input.getType() == Material.GRAVEL && (!input.hasItemMeta() || (input.hasItemMeta() && !input.getItemMeta().hasDisplayName()))) {
+		if (SlimefunManager.isItemSimiliar(input, new ItemStack(Material.GRAVEL), true)) {
 			if (p.getGameMode() != GameMode.CREATIVE) ItemUtils.consumeItem(input, false);
 			
-			for (int i = 1; i < 7; i++) {
-				int j = i;
-				
-				Bukkit.getScheduler().runTaskLater(SlimefunPlugin.instance, () -> {
-					b.getWorld().playEffect(b.getRelative(BlockFace.DOWN).getLocation(), Effect.STEP_SOUND, Material.GRAVEL);
+			ItemStack output = getRandomDrop();
+			TaskQueue queue = new TaskQueue();
+			
+			queue.thenRepeatEvery(20, 5, () ->
+				b.getWorld().playEffect(b.getRelative(BlockFace.DOWN).getLocation(), Effect.STEP_SOUND, Material.GRAVEL)
+			);
+			
+			queue.thenRun(20, () -> {
+				if (output != null) {
+					Inventory outputChest = findOutputChest(b.getRelative(BlockFace.DOWN), output);
 					
-					if (j == 6) {
-						if (drop != null) b.getWorld().dropItemNaturally(b.getLocation(), drop);
-						p.getWorld().playSound(p.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1F, 1F);
+					if (outputChest != null) {
+						outputChest.addItem(output);
+					}
+					else {
+						b.getWorld().dropItemNaturally(b.getLocation(), output);
 					}
 					
-				}, i * 30L);
-				
-			}
+					p.getWorld().playSound(p.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1F, 1F);
+				}
+				else {
+					p.getWorld().playSound(p.getLocation(), Sound.ENTITY_ARMOR_STAND_BREAK, 1F, 1F);
+				}
+			});
 			
-			return;
+			queue.execute(SlimefunPlugin.instance);
+		}
+		else {
+			SlimefunPlugin.getLocal().sendMessage(p, "machines.wrong-item", true);
+		}
+	}
+
+	private ItemStack getRandomDrop() {
+		if (random.nextInt(100) < chanceSiftedOre) {
+			return SlimefunItems.SIFTED_ORE;
+		}
+		else if (random.nextInt(100) < chanceClay) {
+			return new ItemStack(Material.CLAY_BALL);
+		}
+		else if (random.nextInt(100) < chanceFlint) {
+			return new ItemStack(Material.FLINT);
 		}
 		
-		SlimefunPlugin.getLocal().sendMessage(p, "machines.wrong-item", true);
+		return null;
 	}
 
 }
