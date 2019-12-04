732ae5e0dc3878bb330750f0ac82fb11cd1069d6
SoSeDiK <mrsosedik@gmail.com>
2019-12-04 13:35:08 +0200

    Added a new constructor
    Allows using the new item system.

--- src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/HandledBlock.java
+++ src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/HandledBlock.java
@@ -2,6 +2,7 @@ package me.mrCookieSlime.Slimefun.Objects.SlimefunItem;
 
 import org.bukkit.inventory.ItemStack;
 
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 
@@ -9,6 +10,10 @@ public class HandledBlock extends SlimefunItem {
 
 	public HandledBlock(Category category, ItemStack item, String id, RecipeType recipeType, ItemStack[] recipe) {
 		super(category, item, id, recipeType, recipe);
+	}
+
+	public HandledBlock(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+		super(category, item, recipeType, recipe);
 	}
 
 }
