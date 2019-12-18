9517737883147e611629d82967a188e672880a87
J3fftw1 <44972470+J3fftw1@users.noreply.github.com>
2019-12-18 18:17:03 +0100

    added a suggestion

--- src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/multiblocks/OreCrusher.java
+++ src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/multiblocks/OreCrusher.java
@@ -24,36 +24,39 @@ public class OreCrusher extends MultiBlockMachine {
 
 	public OreCrusher() {
 		super(
-				Categories.MACHINES_1, 
-				SlimefunItems.ORE_CRUSHER, 
+				Categories.MACHINES_1,
+				SlimefunItems.ORE_CRUSHER,
 				"ORE_CRUSHER",
 				new ItemStack[] {null, null, null, null, new ItemStack(Material.NETHER_BRICK_FENCE), null, new ItemStack(Material.IRON_BARS), new CustomItem(Material.DISPENSER, "Dispenser (Facing up)"), new ItemStack(Material.IRON_BARS)},
 				new ItemStack[] {
-						new ItemStack(Material.IRON_ORE), new CustomItem(SlimefunItems.IRON_DUST, (boolean) Slimefun.getItemValue("ORE_CRUSHER", "double-ores") ? 2: 1), 
-						new ItemStack(Material.GOLD_ORE), new CustomItem(SlimefunItems.GOLD_DUST, (boolean) Slimefun.getItemValue("ORE_CRUSHER", "double-ores") ? 2: 1), 
-						new ItemStack(Material.NETHERRACK, 16), SlimefunItems.SULFATE, 
-						SlimefunItems.SIFTED_ORE, SlimefunItems.CRUSHED_ORE, 
-						SlimefunItems.CRUSHED_ORE, SlimefunItems.PULVERIZED_ORE, 
-						SlimefunItems.PURE_ORE_CLUSTER, SlimefunItems.TINY_URANIUM, 
-						new ItemStack(Material.COBBLESTONE, 8), new ItemStack(Material.SAND, 1), 
-						new ItemStack(Material.GOLD_INGOT), SlimefunItems.GOLD_DUST, 
+						new ItemStack(Material.IRON_ORE), new CustomItem(SlimefunItems.IRON_DUST, (boolean) Slimefun.getItemValue("ORE_CRUSHER", "double-ores") ? 2: 1),
+						new ItemStack(Material.GOLD_ORE), new CustomItem(SlimefunItems.GOLD_DUST, (boolean) Slimefun.getItemValue("ORE_CRUSHER", "double-ores") ? 2: 1),
+						new ItemStack(Material.NETHERRACK, 16), SlimefunItems.SULFATE,
+						SlimefunItems.SIFTED_ORE, SlimefunItems.CRUSHED_ORE,
+						SlimefunItems.CRUSHED_ORE, SlimefunItems.PULVERIZED_ORE,
+						SlimefunItems.PURE_ORE_CLUSTER, SlimefunItems.TINY_URANIUM,
+						new ItemStack(Material.COBBLESTONE, 8), new ItemStack(Material.SAND, 1),
+						new ItemStack(Material.GOLD_INGOT), SlimefunItems.GOLD_DUST,
 						SlimefunItems.GOLD_4K, SlimefunItems.GOLD_DUST,
-						new ItemStack(Material.GRAVEL), new ItemStack(Material.SAND)
+						new ItemStack(Material.GRAVEL), new ItemStack(Material.SAND),
+						new ItemStack(Material.ANDESITE), new ItemStack(Material.GRAVEL),
+						new ItemStack(Material.DIORITE), new ItemStack(Material.GRAVEL),
+						new ItemStack(Material.GRANITE), new ItemStack(Material.GRAVEL)
 				},
 				BlockFace.SELF
 		);
 	}
-	
+
 	@Override
 	public List<ItemStack> getDisplayRecipes() {
 		return recipes.stream().map(items -> items[0]).collect(Collectors.toList());
 	}
-	
+
 	@Override
 	public void install() {
 		// Do nothing to prevent double-registration of recipes
 	}
-	
+
 	@Override
 	public void onInteract(Player p, Block b) {
 		Block dispBlock = b.getRelative(BlockFace.DOWN);
@@ -73,12 +76,12 @@ public class OreCrusher extends MultiBlockMachine {
 						p.getWorld().playEffect(b.getLocation(), Effect.STEP_SOUND, 1);
 					}
 					else SlimefunPlugin.getLocal().sendMessage(p, "machines.full-inventory", true);
-					
+
 					return;
 				}
 			}
 		}
-		
+
 		SlimefunPlugin.getLocal().sendMessage(p, "machines.unknown-material", true);
 	}
 
