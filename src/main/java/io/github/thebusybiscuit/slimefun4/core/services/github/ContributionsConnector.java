1db1168c092b2194ce5fe80db545c7a3924bf7d9
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-01-31 00:59:36 +0100

    [CI skip] Added more future language names

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
@@ -43,6 +43,7 @@ public class ContributionsConnector extends GitHubConnector {
 		aliases.put("bverhoeven", "soczol");
 		aliases.put("ramdon-person", "ramdon_person");
 		aliases.put("mohkamfer", "citBabY");
+		aliases.put("JustDams", "D4ms_");
 	}
 	
 	private final String prefix;
