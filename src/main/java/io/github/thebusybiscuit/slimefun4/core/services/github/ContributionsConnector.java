b5a7b5f5b69d4067867569d99e11bee307a7014b
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-02-10 13:45:50 +0100

    [CI skip] Removed a redundancy

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
@@ -42,7 +42,6 @@ public class ContributionsConnector extends GitHubConnector {
 		aliases.put("BurningBrimstone", "Bluedevil74");
 		aliases.put("bverhoeven", "soczol");
 		aliases.put("ramdon-person", "ramdon_person");
-		aliases.put("mohkamfer", "citBabY");
 	}
 	
 	private final String prefix;
