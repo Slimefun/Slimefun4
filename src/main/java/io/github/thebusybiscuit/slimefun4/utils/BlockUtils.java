94614474101c9db20fbe24311783d64244ed7622
DNx <dnxnim@gmail.com>
2020-04-05 23:30:19 +0700

    split shulker boxes check

--- src/main/java/io/github/thebusybiscuit/slimefun4/utils/BlockUtils.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/utils/BlockUtils.java
@@ -1,4 +1,5 @@
 package io.github.thebusybiscuit.slimefun4.utils;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 public final class BlockUtils {
 
@@ -15,23 +16,6 @@ public final class BlockUtils {
             case DROPPER:
             case HOPPER:
             case BREWING_STAND:
-            case SHULKER_BOX:
-            case WHITE_SHULKER_BOX:
-            case ORANGE_SHULKER_BOX:
-            case MAGENTA_SHULKER_BOX:
-            case LIGHT_BLUE_SHULKER_BOX:
-            case YELLOW_SHULKER_BOX:
-            case LIME_SHULKER_BOX:
-            case PINK_SHULKER_BOX:
-            case GRAY_SHULKER_BOX:
-            case LIGHT_GRAY_SHULKER_BOX:
-            case CYAN_SHULKER_BOX:
-            case PURPLE_SHULKER_BOX:
-            case BLUE_SHULKER_BOX:
-            case BROWN_SHULKER_BOX:
-            case GREEN_SHULKER_BOX:
-            case RED_SHULKER_BOX:
-            case BLACK_SHULKER_BOX:
             case ENDER_CHEST:
             case BARREL:
             case BLAST_FURNACE:
@@ -39,8 +23,19 @@ public final class BlockUtils {
             case SMOKER:
                 return true;
             default:
-                return false;
+                return isShulkerBox(block);
         }
+    }
+
+    public static boolean isShulkerBox(Block block) {
+        Material type = block.getType();
+        return type == Material.SHULKER_BOX || type == Material.WHITE_SHULKER_BOX || type == Material.ORANGE_SHULKER_BOX
+                || type == Material.MAGENTA_SHULKER_BOX || type == Material.LIGHT_BLUE_SHULKER_BOX
+                || type == Material.YELLOW_SHULKER_BOX || type == Material.LIME_SHULKER_BOX
+                || type == Material.PINK_SHULKER_BOX || type == Material.GRAY_SHULKER_BOX
+                || type == Material.LIGHT_GRAY_SHULKER_BOX || type == Material.CYAN_SHULKER_BOX
+                || type == Material.PURPLE_SHULKER_BOX || type == Material.BLUE_SHULKER_BOX || type == Material.BROWN_SHULKER_BOX
+                || type == Material.GREEN_SHULKER_BOX || type == Material.RED_SHULKER_BOX || type == Material.BLACK_SHULKER_BOX;
     }
 
 }
