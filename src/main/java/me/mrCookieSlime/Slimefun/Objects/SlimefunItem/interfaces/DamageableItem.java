a5afa21b738071d870714057043e1a9a31049592
StarWishsama <starwishsama@outlook.com>
2020-04-07 16:33:45 +0800

    [U] 上传未同步的文件

---
+++ src/main/java/me/mrCookieSlime/Slimefun/Objects/SlimefunItem/interfaces/DamageableItem.java
@@ -0,0 +1,9 @@
+package me.mrCookieSlime.Slimefun.Objects.SlimefunItem.interfaces;
+
+/**
+ * @deprecated Moved to {@code io.github.thebusybiscuit.slimefun4.core.attributes.DamageableItem}
+ */
+@Deprecated
+public interface DamageableItem extends io.github.thebusybiscuit.slimefun4.core.attributes.DamageableItem {
+
+}
