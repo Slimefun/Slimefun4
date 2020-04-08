44d96ac339afcf408ccf8926fc52fb7bb52abc1a
DetectorMe <62680602+DetectorMe@users.noreply.github.com>
2020-04-08 15:32:24 +0800

    修复冶炼炉和火焰不熄灭和对应的报错

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/Smeltery.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/Smeltery.java
@@ -37,6 +37,7 @@ public class Smeltery extends MultiBlockMachine {
 
     public Smeltery() {
         super(Categories.MACHINES_1, SlimefunItems.SMELTERY, new ItemStack[]{null, new ItemStack(Material.NETHER_BRICK_FENCE), null, new ItemStack(Material.NETHER_BRICKS), new CustomItem(Material.DISPENSER, "Dispenser (Facing up)"), new ItemStack(Material.NETHER_BRICKS), null, new ItemStack(Material.FLINT_AND_STEEL), null}, new ItemStack[]{SlimefunItems.IRON_DUST, new ItemStack(Material.IRON_INGOT)}, BlockFace.DOWN);
+        addItemSetting(fireBreakingChance);
     }
 
     @Override
