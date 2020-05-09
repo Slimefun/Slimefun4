f5980898269a70339988bc6e909ff0b32d4abbdc
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-05-09 10:24:19 +0200

    Fixed Multiblocks tests

--- src/test/java/io/github/thebusybiscuit/slimefun4/tests/multiblocks/TestMultiBlocks.java
+++ src/test/java/io/github/thebusybiscuit/slimefun4/tests/multiblocks/TestMultiBlocks.java
@@ -2,15 +2,28 @@ package io.github.thebusybiscuit.slimefun4.tests.multiblocks;
 
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import be.seeseemelk.mockbukkit.MockBukkit;
 import io.github.thebusybiscuit.cscorelib2.item.CustomItem;
 import io.github.thebusybiscuit.slimefun4.core.MultiBlock;
 import io.github.thebusybiscuit.slimefun4.mocks.SlimefunMocks;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 
 public class TestMultiBlocks {
+
+    @BeforeAll
+    public static void load() {
+        MockBukkit.mock();
+    }
+
+    @AfterAll
+    public static void unload() {
+        MockBukkit.unmock();
+    }
 
     @Test
     public void testInvalidConstructors() {
@@ -38,19 +51,11 @@ public class TestMultiBlocks {
     public void testSymmetry() {
         SlimefunItem item = SlimefunMocks.mockSlimefunItem("MULTIBLOCK_TEST", new CustomItem(Material.BRICK, "&5Multiblock Test"));
 
-        MultiBlock multiblock = new MultiBlock(item, new Material[] {
-                null, null, null,
-                Material.DIAMOND_BLOCK, null, Material.DIAMOND_BLOCK,
-                null, Material.DISPENSER, null
-        }, BlockFace.DOWN);
+        MultiBlock multiblock = new MultiBlock(item, new Material[] { null, null, null, Material.DIAMOND_BLOCK, null, Material.DIAMOND_BLOCK, null, Material.DISPENSER, null }, BlockFace.DOWN);
 
         Assertions.assertTrue(multiblock.isSymmetric());
-        
-        MultiBlock multiblock2 = new MultiBlock(item, new Material[] {
-                Material.EMERALD_BLOCK, null, null,
-                Material.EMERALD_BLOCK, null, Material.DIAMOND_BLOCK,
-                Material.EMERALD_BLOCK, Material.DISPENSER, null
-        }, BlockFace.DOWN);
+
+        MultiBlock multiblock2 = new MultiBlock(item, new Material[] { Material.EMERALD_BLOCK, null, null, Material.EMERALD_BLOCK, null, Material.DIAMOND_BLOCK, Material.EMERALD_BLOCK, Material.DISPENSER, null }, BlockFace.DOWN);
 
         Assertions.assertFalse(multiblock2.isSymmetric());
     }
@@ -59,35 +64,15 @@ public class TestMultiBlocks {
     public void testEquality() {
         SlimefunItem item = SlimefunMocks.mockSlimefunItem("MULTIBLOCK_TEST", new CustomItem(Material.BRICK, "&5Multiblock Test"));
 
-        MultiBlock multiblock = new MultiBlock(item, new Material[] {
-                Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD,
-                null, Material.CRAFTING_TABLE, null,
-                Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD
-        }, BlockFace.DOWN);
-        
-        MultiBlock multiblock2 = new MultiBlock(item, new Material[] {
-                Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD,
-                null, Material.CRAFTING_TABLE, null,
-                Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD
-        }, BlockFace.DOWN);
-        
-        MultiBlock multiblock3 = new MultiBlock(item, new Material[] {
-                Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD,
-                null, Material.EMERALD_BLOCK, null,
-                Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD
-        }, BlockFace.DOWN);
-        
-        MultiBlock multiblock4 = new MultiBlock(item, new Material[] {
-                Material.DROPPER, Material.BIRCH_WOOD, Material.BIRCH_WOOD,
-                null, Material.DIAMOND_BLOCK, null,
-                Material.BIRCH_WOOD, Material.DISPENSER, Material.TNT
-        }, BlockFace.DOWN);
-        
-        MultiBlock multiblock5 = new MultiBlock(item, new Material[] {
-                Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD,
-                null, Material.CRAFTING_TABLE, null,
-                Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD
-        }, BlockFace.SELF);
+        MultiBlock multiblock = new MultiBlock(item, new Material[] { Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD, null, Material.CRAFTING_TABLE, null, Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD }, BlockFace.DOWN);
+
+        MultiBlock multiblock2 = new MultiBlock(item, new Material[] { Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD, null, Material.CRAFTING_TABLE, null, Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD }, BlockFace.DOWN);
+
+        MultiBlock multiblock3 = new MultiBlock(item, new Material[] { Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD, null, Material.EMERALD_BLOCK, null, Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD }, BlockFace.DOWN);
+
+        MultiBlock multiblock4 = new MultiBlock(item, new Material[] { Material.DROPPER, Material.BIRCH_WOOD, Material.BIRCH_WOOD, null, Material.DIAMOND_BLOCK, null, Material.BIRCH_WOOD, Material.DISPENSER, Material.TNT }, BlockFace.DOWN);
+
+        MultiBlock multiblock5 = new MultiBlock(item, new Material[] { Material.BIRCH_WOOD, Material.BIRCH_WOOD, Material.BIRCH_WOOD, null, Material.CRAFTING_TABLE, null, Material.BIRCH_WOOD, Material.DISPENSER, Material.BIRCH_WOOD }, BlockFace.SELF);
 
         Assertions.assertTrue(multiblock.isSymmetric());
 
