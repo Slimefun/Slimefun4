64f57c26256d04b091e316622d5764104975716d
StarWishsama <starwishsama@outlook.com>
2020-06-07 11:44:04 +0800

    [O] 优化自动更新

--- src/main/java/io/github/starwishsama/extra/SlimefunUpdater.java
+++ src/main/java/io/github/starwishsama/extra/SlimefunUpdater.java
@@ -84,9 +84,10 @@ public class SlimefunUpdater {
                 if (current >= latest && splitVersion.length == 3) {
                     Slimefun.getLogger().info(ChatColors.color("&a你正在使用最新版本 " + SlimefunPlugin.getVersion()));
                 } else {
-                    String updateInfo = "有更新了 &7| &b" + bean.getTagName() + " 现已发布\n正在自动下载更新中, 下载完成后重启服务器生效";
+                    String updateInfo = "有更新了 | " + bean.getTagName() + " 现已发布\n正在自动下载更新中, 下载完成后重启服务器生效";
                     Slimefun.getLogger().info(updateInfo);
                     UpdateDownloader.downloadUpdate(getCache().getAssets().get(0).getDownloadUrl(), getCache().getAssets().get(0).getName());
+                    return;
                 }
             }
         }
--- src/main/java/io/github/starwishsama/extra/UpdateDownloader.java
+++ src/main/java/io/github/starwishsama/extra/UpdateDownloader.java
@@ -4,7 +4,10 @@ import io.github.thebusybiscuit.cscorelib2.chat.ChatColors;
 import me.mrCookieSlime.Slimefun.SlimefunPlugin;
 import me.mrCookieSlime.Slimefun.api.Slimefun;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Level;
@@ -16,39 +19,36 @@ public class UpdateDownloader {
     public static void downloadUpdate(String address, String fileName) {
         try {
             URL url = new URL(address);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) (url.openConnection());
             conn.setConnectTimeout(5_000);
             conn.setReadTimeout(5_000);
             conn.setDoOutput(true);
             conn.setRequestProperty("User-Agent", browserUA);
 
-            InputStream in = conn.getInputStream();
-            byte[] data = readInputStream(conn.getInputStream());
+            long completeFileSize = conn.getContentLength();
+
+            BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
             File saveDir = new File(downloadDir.replace("update", "plugins"));
             File file = new File(saveDir, fileName);
             FileOutputStream fos = new FileOutputStream(file);
-            fos.write(data);
-
-            fos.close();
+            BufferedOutputStream bos = new BufferedOutputStream(fos, 1024);
+            byte[] data = new byte[1024];
+            long downloadedFileSize = 0;
+            int x;
+            while ((x = in.read(data, 0, 1024)) >= 0) {
+                downloadedFileSize += x;
+                final int currentProgress = (int) ((((double) downloadedFileSize) / ((double) completeFileSize)) * 100000d);
+                Slimefun.getLogger().info("下载中, 进度 " + currentProgress + "%");
+                bos.write(data, 0, x);
+            }
+            bos.close();
             in.close();
+            fos.close();
 
             SlimefunPlugin.instance.getFile().deleteOnExit();
-
             Slimefun.getLogger().info(ChatColors.color("&a自动更新已完成, 重启服务端后即可更新到最新版本"));
         } catch (Exception e) {
             Slimefun.getLogger().log(Level.SEVERE, e, () -> "在下载时发生了错误");
         }
-    }
-
-    private static byte[] readInputStream(InputStream inputStream) throws IOException {
-        byte[] buffer = new byte[1024];
-        int leng;
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-        while ((leng = inputStream.read(buffer)) != -1) {
-            b.write(buffer, 0, leng);
-        }
-        b.close();
-
-        return b.toByteArray();
     }
 }
