e52ac9d43b49cd451d8000e3c71a1603d021c2ba
SoSeDiK <mrsosedik@gmail.com>
2020-06-25 13:06:34 +0300

    Make it compile

--- src/main/java/me/mrCookieSlime/Slimefun/SlimefunPlugin.java
+++ src/main/java/me/mrCookieSlime/Slimefun/SlimefunPlugin.java
@@ -1,7 +1,12 @@
 package me.mrCookieSlime.Slimefun;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
