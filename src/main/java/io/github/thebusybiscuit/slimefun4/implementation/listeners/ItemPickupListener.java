9fb79c86225eb28824858ff30f908ea0a35ea452
poma123 <vhvvvhhh@gmail.com>
2020-07-25 12:04:22 +0200

    Fixed piglin SlimefunItem pickup

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/ItemPickupListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/ItemPickupListener.java
@@ -1,5 +1,8 @@
 package io.github.thebusybiscuit.slimefun4.implementation.listeners;
 
+import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
@@ -10,8 +13,9 @@ import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 
 /**
- * Listens to the ItemPickup events to prevent it if the item has the "no_pickup" metadata or is an ALTAR_PROBE.
- * 
+ * Listens to the ItemPickup events to prevent it if the item has the "no_pickup" metadata or is an ALTAR_PROBE
+ * or if a piglin wants to pickup SlimefunItems.
+ *
  * @author TheBusyBiscuit
  */
 public class ItemPickupListener implements Listener {
@@ -30,6 +34,12 @@ public class ItemPickupListener implements Listener {
         else if (e.getItem().getItemStack().hasItemMeta() && e.getItem().getItemStack().getItemMeta().hasDisplayName() && e.getItem().getItemStack().getItemMeta().getDisplayName().startsWith(ITEM_PREFIX)) {
             e.setCancelled(true);
             e.getItem().remove();
+        } else if (SlimefunPlugin.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_16)) {
+            if (e.getEntityType().equals(EntityType.PIGLIN) || e.getEntityType().equals(EntityType.ZOMBIFIED_PIGLIN)) {
+                if (SlimefunItem.getByItem(e.getItem().getItemStack()) != null) {
+                    e.setCancelled(true);
+                }
+            }
         }
     }
 
