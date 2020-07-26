260a6f4f6160106319ead9ec725d92d4278549ed
dniym <sales@whoolie.com>
2020-07-26 10:51:18 -0400

    forgot one

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/attributes/RandomMobDrop.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/attributes/RandomMobDrop.java
@@ -12,8 +12,8 @@ import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 
 /**
  * This interface, when attached to a {@link SlimefunItem}, provides an easy method for adding
- * a % chance to drop for an {@link SlimefunItem} on {@link entityDeathEvent} or {@link EntityItemDropEvent}, this chance is 0-100
- * and used in conjunction with the MOB_DROP {@link RecipeType} or the BARTER_DROP {@link RecipeType}.
+ * a % chance to drop for an {@link SlimefunItem} on {@link entityDeathEvent}, this chance is 0-100
+ * and used in conjunction with the MOB_DROP {@link RecipeType} 
  * 
  * @author dNiym
  *
@@ -28,14 +28,6 @@ public interface RandomMobDrop extends ItemAttribute {
      * Implement this method to make the object have a variable chance of being
      * added to the dropList when {@link EntityType} (specified in the recipe)
      * is killed by the {@link Player} for a MOB_DROP {@link RecipeType}.
-     * 
-     * Alternatively if the {@link RecipeType} is set to BARTER_DROP the item
-     * will then have its % chance to be dropped only by bartering with the
-     * Piglin creatures.
-     * 
-     * It is recommended that this chance is kept reasonably low to feel like
-     * a vanilla drop as a 100% chance will completely override all piglin 
-     * barter drops.  (NOTE: this feature only exists in 1.16+)
      * 
      * @return The integer chance (0-100%) {@link SlimefunItem} has to drop.
      */
