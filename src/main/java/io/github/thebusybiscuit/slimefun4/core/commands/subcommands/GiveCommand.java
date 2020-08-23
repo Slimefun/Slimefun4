eb66f3e183062c88925ec47d5d5a90a25275a5fc
Sfiguz7 <ale.colli98@gmail.com>
2020-08-23 13:54:51 +0200

    Added option to drop excess items on /sf give

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/GiveCommand.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/GiveCommand.java
@@ -1,8 +1,11 @@
 package io.github.thebusybiscuit.slimefun4.core.commands.subcommands;
 
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Optional;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -14,6 +17,7 @@ import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlockMachine;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
 import io.github.thebusybiscuit.slimefun4.utils.PatternUtils;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import org.bukkit.inventory.ItemStack;
 
 class GiveCommand extends SubCommand {
 
@@ -65,7 +69,13 @@ class GiveCommand extends SubCommand {
 
             if (amount > 0) {
                 SlimefunPlugin.getLocalization().sendMessage(p, "messages.given-item", true, msg -> msg.replace(PLACEHOLDER_ITEM, sfItem.getItemName()).replace(PLACEHOLDER_AMOUNT, String.valueOf(amount)));
-                p.getInventory().addItem(new CustomItem(sfItem.getItem(), amount));
+                HashMap<Integer,ItemStack> excess = p.getInventory().addItem(new CustomItem(sfItem.getItem(), amount));
+                if (SlimefunPlugin.getCfg().getBoolean("options.drop-excess-sf-give-items") && !excess.isEmpty()) {
+                        for (ItemStack is : excess.values()) {
+                            p.getWorld().dropItemNaturally(p.getLocation(), is);
+                        }
+                    }
+
                 SlimefunPlugin.getLocalization().sendMessage(sender, "messages.give-item", true, msg -> msg.replace(PLACEHOLDER_PLAYER, args[1]).replace(PLACEHOLDER_ITEM, sfItem.getItemName()).replace(PLACEHOLDER_AMOUNT, String.valueOf(amount)));
             }
             else {
