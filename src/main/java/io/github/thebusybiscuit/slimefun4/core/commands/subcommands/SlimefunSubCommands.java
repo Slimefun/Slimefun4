cf1c58dd8bad4527fa54341f8cb7cdbf18875758
FluffyBear <unconfigured@null.spigotmc.org>
2020-09-04 00:54:06 -0500

    Got rid of charge command from this branch

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/SlimefunSubCommands.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/SlimefunSubCommands.java
@@ -37,7 +37,6 @@ public final class SlimefunSubCommands {
         commands.add(new SearchCommand(plugin, cmd));
         commands.add(new DebugFishCommand(plugin, cmd));
         commands.add(new BackpackCommand(plugin, cmd));
-        commands.add(new ChargeCommand(plugin, cmd));
 
         return commands;
     }
