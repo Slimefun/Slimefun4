f0cbd3fc3a00220b712f0e6c27ce57ee1b252edb
TheSilentPro <perovskiboki@mail.com>
2020-09-22 23:25:19 +0200

    Forgot to commit the research

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/ResearchSetup.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/ResearchSetup.java
@@ -275,6 +275,7 @@ public final class ResearchSetup {
         register("villager_rune", 264, "Reset Villager Trades", 26, SlimefunItems.VILLAGER_RUNE, SlimefunItems.STRANGE_NETHER_GOO);
         register("climbing_pick", 265, "Block Raider", 20, SlimefunItems.CLIMBING_PICK);
         register("even_higher_tier_capacitors", 266, "Tier 3 Capacitors", 40, SlimefunItems.ENERGIZED_CAPACITOR);
+        register("containment_backpack", 267, "Containment Backpack", 30, SlimefunItems.CONTAINMENT_BACKPACK);
     }
 
     private static void register(String key, int id, String name, int defaultCost, ItemStack... items) {
