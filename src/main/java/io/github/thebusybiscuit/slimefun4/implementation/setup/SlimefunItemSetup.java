a00e640a6c012a6805f3d33c94657583dbf62930
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-10-02 19:53:01 +0200

    Add Talisman of the Hastier

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/SlimefunItemSetup.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/setup/SlimefunItemSetup.java
@@ -872,6 +872,11 @@ public final class SlimefunItemSetup {
         "knight", 30, new PotionEffect(PotionEffectType.REGENERATION, 100, 3))
         .register(plugin);
 
+        new Talisman(SlimefunItems.TALISMAN_HASTIER,
+        new ItemStack[] { SlimefunItems.MAGIC_LUMP_3, null, SlimefunItems.MAGIC_LUMP_3, new ItemStack(Material.GOLDEN_PICKAXE), SlimefunItems.TALISMAN_MINER, SlimefunItems.EARTH_RUNE, SlimefunItems.MAGIC_LUMP_3, null, SlimefunItems.MAGIC_LUMP_3},
+        false, false, "hastier", new PotionEffect(PotionEffectType.FAST_DIGGING, 180, 3))
+        .register(plugin);
+
         new SlimefunItem(categories.resources, SlimefunItems.GILDED_IRON, RecipeType.SMELTERY,
         new ItemStack[] {SlimefunItems.GOLD_24K, SlimefunItems.IRON_DUST, null, null, null, null, null, null, null})
         .register(plugin);
