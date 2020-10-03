41fc987083d4dae8c171282518832fd26a2d5349
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-10-03 23:56:51 +0300

    Did more requested changes.

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/events/SlimefunGuideOpenEvent.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/events/SlimefunGuideOpenEvent.java
@@ -9,35 +9,58 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.thebusybiscuit.slimefun4.core.guide.SlimefunGuideLayout;
+
 /**
  * This {@link Event} is called whenever a {@link Player} tries to open the Slimefun Guide book. 
  * 
  * @author Linox
  *
+ * @see SlimefunGuideLayout
  */
-public class GuideOpenEvent extends Event implements Cancellable {
+public class SlimefunGuideOpenEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
 
     private final Player player;
     private final ItemStack guide;
+    private final SlimefunGuideLayout layout;
     private boolean cancelled;
 
-    public GuideOpenEvent(@Nonnull Player p, @Nonnull ItemStack guide) {
+    public SlimefunGuideOpenEvent(@Nonnull Player p, @Nonnull ItemStack guide, @Nonnull SlimefunGuideLayout layout) {
         Validate.notNull(p, "The Player cannot be null");
         Validate.notNull(guide, "Guide cannot be null");
+        Validate.notNull(layout, "Layout cannot be null");
         this.player = p;
         this.guide = guide;
+        this.layout = layout;
     }
 
+    /**
+     * This returns the {@link Player} that tries to open 
+     * the Slimefun Guide.
+     */
     @Nonnull
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * This returns the {@link ItemStack} that {@link Player} 
+     * tries to open the Slimefun Guide with.
+     */
     @Nonnull
     public ItemStack getGuide() {
         return guide;
+    }
+    
+    /**
+     * This returns the {@link SlimefunGuideLayout} of the Slimefun Guide
+     * that {@link Player} tries to open.
+     */
+    @Nonnull
+    public SlimefunGuideLayout getGuideLayout() {
+        return layout;
     }
 
     @Override
