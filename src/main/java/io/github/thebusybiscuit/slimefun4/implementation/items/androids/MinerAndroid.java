4d9a50a12f7ff8a83e47093eb7a6be52c6a798a2
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-10-11 10:02:51 +0300

    Some refactoring.

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/MinerAndroid.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/MinerAndroid.java
@@ -3,6 +3,9 @@ package io.github.thebusybiscuit.slimefun4.implementation.items.androids;
 import java.util.Collection;
 import java.util.UUID;
 
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Material;
@@ -22,21 +25,33 @@ import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 
+/**
+ * Represents a {@link MinerAndroid Programmable Android (Miner)}.
+ * This {@link ProgrammableAndroid} can dig blocks and move.
+ *
+ * @author Linox
+ *
+ * @see AndroidMineEvent
+ *
+ */
 public class MinerAndroid extends ProgrammableAndroid {
 
     // Determines the drops a miner android will get
     private final ItemStack effectivePickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
 
+    @ParametersAreNonnullByDefault
     public MinerAndroid(Category category, int tier, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, tier, item, recipeType, recipe);
     }
 
     @Override
+    @Nonnull
     public AndroidType getAndroidType() {
         return AndroidType.MINER;
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     protected void dig(Block b, BlockMenu menu, Block block) {
         Collection<ItemStack> drops = block.getDrops(effectivePickaxe);
 
@@ -44,16 +59,17 @@ public class MinerAndroid extends ProgrammableAndroid {
             OfflinePlayer owner = Bukkit.getOfflinePlayer(UUID.fromString(BlockStorage.getLocationInfo(b.getLocation(), "owner")));
 
             if (SlimefunPlugin.getProtectionManager().hasPermission(owner, block.getLocation(), ProtectableAction.BREAK_BLOCK)) {
-                AndroidMineEvent event = new AndroidMineEvent(block, new AndroidInstance(this, b));
-                Bukkit.getPluginManager().callEvent(event);
 
-                if (event.isCancelled()) {
+                // We only want to break non-Slimefun blocks.
+                String blockId = BlockStorage.checkID(block);
+                if (blockId != null) {
                     return;
                 }
 
-                // We only want to break non-Slimefun blocks
-                String blockId = BlockStorage.checkID(block);
-                if (blockId == null) {
+                AndroidMineEvent event = new AndroidMineEvent(block, new AndroidInstance(this, b));
+                Bukkit.getPluginManager().callEvent(event);
+
+                if (!event.isCancelled()) {
                     for (ItemStack drop : drops) {
                         if (menu.fits(drop, getOutputSlots())) {
                             menu.pushItem(drop, getOutputSlots());
@@ -68,40 +84,10 @@ public class MinerAndroid extends ProgrammableAndroid {
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     protected void moveAndDig(Block b, BlockMenu menu, BlockFace face, Block block) {
-        Collection<ItemStack> drops = block.getDrops(effectivePickaxe);
-
-        if (!MaterialCollections.getAllUnbreakableBlocks().contains(block.getType()) && !drops.isEmpty()) {
-            OfflinePlayer owner = Bukkit.getOfflinePlayer(UUID.fromString(BlockStorage.getLocationInfo(b.getLocation(), "owner")));
-
-            if (SlimefunPlugin.getProtectionManager().hasPermission(owner, block.getLocation(), ProtectableAction.BREAK_BLOCK)) {
-                AndroidMineEvent event = new AndroidMineEvent(block, new AndroidInstance(this, b));
-                Bukkit.getPluginManager().callEvent(event);
-
-                if (event.isCancelled()) {
-                    return;
-                }
-
-                // We only want to break non-Slimefun blocks
-                SlimefunItem blockId = BlockStorage.check(block);
-                if (blockId == null) {
-                    for (ItemStack drop : drops) {
-                        if (menu.fits(drop, getOutputSlots())) {
-                            menu.pushItem(drop, getOutputSlots());
-                        }
-                    }
-
-                    block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, block.getType());
-
-                    block.setType(Material.AIR);
-                    move(b, face, block);
-                }
-            } else {
-                move(b, face, block);
-            }
-        } else {
-            move(b, face, block);
-        }
+        dig(b, menu, block);
+        move(b, face, block);
     }
 
 }
