c4a08fe99119e919fe4486db2165df823c2ae35f
CURVX <chakrawarti.anupam@gmail.com>
2020-11-12 18:32:18 +0530

    Changes req by TTB

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/crafting/SlimefunCraftingListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/crafting/SlimefunCraftingListener.java
@@ -27,7 +27,7 @@ interface SlimefunCraftingListener extends Listener {
         }
 
         SlimefunItem sfItem = SlimefunItem.getByItem(item);
-        return sfItem != null && !(sfItem instanceof VanillaItem) && !sfItem.isDisabled();
+        return isUnallowed(sfItem);
     }
 
     default boolean isUnallowed(@Nullable SlimefunItem item) {
