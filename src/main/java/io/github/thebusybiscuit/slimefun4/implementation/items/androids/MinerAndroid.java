3aa02d5e040bb9c6bd1739ac4b8f418a776d90a7
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-03-13 11:22:59 +0100

    Added an option to disable block-gen optimizations for androids

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/MinerAndroid.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/MinerAndroid.java
@@ -3,6 +3,7 @@ package io.github.thebusybiscuit.slimefun4.implementation.items.androids;
 import java.util.Collection;
 import java.util.UUID;
 
+import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import org.bukkit.Bukkit;
@@ -27,11 +28,33 @@ import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 
+/**
+ * The {@link MinerAndroid} is a variant of the {@link ProgrammableAndroid} which
+ * is able to break blocks.
+ * The core functionalities boil down to {@link #dig(Block, BlockMenu, Block)} and
+ * {@link #moveAndDig(Block, BlockMenu, BlockFace, Block)}.
+ * Otherwise the functionality is similar to a regular android.
+ * <p>
+ * The {@link MinerAndroid} will also fire an {@link AndroidMineEvent} when breaking a {@link Block}.
+ * 
+ * @author TheBusyBiscuit
+ * @author creator3
+ * @author poma123
+ * @author Sfiguz7
+ * @author CyberPatriot
+ * @author Redemption198
+ * @author Poslovitch
+ * 
+ * @see AndroidMineEvent
+ *
+ */
 public class MinerAndroid extends ProgrammableAndroid {
 
     // Determines the drops a miner android will get
     private final ItemStack effectivePickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
+
     private final ItemSetting<Boolean> firesEvent = new ItemSetting<>("trigger-event-for-generators", false);
+    private final ItemSetting<Boolean> applyOptimizations = new ItemSetting<>("reduced-block-updates", true);
 
     @ParametersAreNonnullByDefault
     public MinerAndroid(Category category, int tier, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
@@ -40,11 +63,13 @@ public class MinerAndroid extends ProgrammableAndroid {
     }
 
     @Override
+    @Nonnull
     public AndroidType getAndroidType() {
         return AndroidType.MINER;
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     protected void dig(Block b, BlockMenu menu, Block block) {
         Collection<ItemStack> drops = block.getDrops(effectivePickaxe);
 
@@ -69,6 +94,7 @@ public class MinerAndroid extends ProgrammableAndroid {
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     protected void moveAndDig(Block b, BlockMenu menu, BlockFace face, Block block) {
         Collection<ItemStack> drops = block.getDrops(effectivePickaxe);
 
@@ -104,16 +130,22 @@ public class MinerAndroid extends ProgrammableAndroid {
             menu.pushItem(drop, getOutputSlots());
         }
 
-        InfiniteBlockGenerator generator = InfiniteBlockGenerator.findAt(block);
+        // Check if Block Generator optimizations should be applied.
+        if (applyOptimizations.getValue()) {
+            InfiniteBlockGenerator generator = InfiniteBlockGenerator.findAt(block);
 
-        if (generator != null) {
-            if (firesEvent.getValue()) {
-                generator.callEvent(block);
+            // If we found a generator, continue.
+            if (generator != null) {
+                if (firesEvent.getValue()) {
+                    generator.callEvent(block);
+                }
+
+                // "poof" a "new" block was generated
+                block.getWorld().playSound(block.getLocation(), Sound.BLOCK_FIRE_EXTINGUISH, 0.075F, 0.8F);
+                block.getWorld().spawnParticle(Particle.SMOKE_NORMAL, block.getX() + 0.5, block.getY() + 1.25, block.getZ() + 0.5, 8, 0.5, 0.5, 0.5, 0.015);
+            } else {
+                block.setType(Material.AIR);
             }
-
-            // "poof" a "new" block was generated
-            block.getWorld().playSound(block.getLocation(), Sound.BLOCK_FIRE_EXTINGUISH, 0.075F, 0.8F);
-            block.getWorld().spawnParticle(Particle.SMOKE_NORMAL, block.getX() + 0.5, block.getY() + 1.25, block.getZ() + 0.5, 8, 0.5, 0.5, 0.5, 0.015);
         } else {
             block.setType(Material.AIR);
         }
