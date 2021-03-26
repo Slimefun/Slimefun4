50aaa08cccbd1480a7772ee1edb8d93f2d8f2c93
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2021-03-27 00:36:20 +0800

    Update Crucible.java

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/blocks/Crucible.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/blocks/Crucible.java
@@ -14,7 +14,6 @@ import org.bukkit.Tag;
 import org.bukkit.World.Environment;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Levelled;
 import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
