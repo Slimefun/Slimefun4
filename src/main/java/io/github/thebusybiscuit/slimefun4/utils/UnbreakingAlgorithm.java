dcf411032ed3b7fee68905395c6871bd0bd06ca6
RobotHanzo <nathanlee302@outlook.com>
2021-04-03 19:25:22 +0800

    Fixes

--- src/main/java/io/github/thebusybiscuit/slimefun4/utils/UnbreakingAlgorithm.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/utils/UnbreakingAlgorithm.java
@@ -3,6 +3,9 @@ package io.github.thebusybiscuit.slimefun4.utils;
 
 import io.github.thebusybiscuit.slimefun4.core.attributes.DamageableItem;
 
+import javax.annotation.Nonnull;
+import java.util.function.IntFunction;
+
 /**
  * This a enum evaluating and indicating a {@link DamageableItem} 's chance to be damaged
  * depending if it is a tool or an armor
@@ -11,7 +14,7 @@ import io.github.thebusybiscuit.slimefun4.core.attributes.DamageableItem;
  */
 public enum UnbreakingAlgorithm {
 
-    ARMOR(lvl -> !(Math.random() < 0.6 + (0.4 / (lvl + 1))),
+    ARMOR(lvl -> !(Math.random() < 0.6 + (0.4 / (lvl + 1)))),
     TOOLS(lvl -> !(Math.random() < (1.0 / (lvl + 1))));
 
     private final IntFunction<Boolean> function;
