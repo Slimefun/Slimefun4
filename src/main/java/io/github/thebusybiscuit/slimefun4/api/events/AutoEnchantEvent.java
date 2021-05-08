bebd89df30b0384c9ab7e7a01df351b9a2b0c26d
StarWishsama <starwishsama@outlook.com>
2021-05-08 13:44:05 +0800

    chore: dont mess up import, idea

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/events/AutoEnchantEvent.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/events/AutoEnchantEvent.java
@@ -1,12 +1,12 @@
 package io.github.thebusybiscuit.slimefun4.api.events;
 
+import javax.annotation.Nonnull;
+
 import io.github.thebusybiscuit.slimefun4.implementation.items.electric.machines.enchanting.AutoEnchanter;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
-
-import javax.annotation.Nonnull;
 
 /**
  * An {@link Event} that is called whenever an {@link AutoEnchanter} is trying to enchant
