b9215c884e2f225e7fd42e07b1cd6d5efb217a4b
xSavior-of-God <66216419+xSavior-of-God@users.noreply.github.com>
2021-05-20 09:04:11 +0200

    Revert "-"
    This reverts commit f3f0f8402560f5125b969079dc2452771183ec6c.

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/WorldListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/WorldListener.java
@@ -4,12 +4,11 @@ import java.util.logging.Level;
 
 import javax.annotation.Nonnull;
 
-import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
+
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 
@@ -19,16 +18,14 @@ public class WorldListener implements Listener {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler
     public void onWorldLoad(WorldLoadEvent e) {
-        System.out.println(ChatColor.RED+"SLIMEFUN- Loading world ... "+e.getWorld().getName());
         SlimefunPlugin.getWorldSettingsService().load(e.getWorld());
         BlockStorage.getOrCreate(e.getWorld());
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler
     public void onWorldUnload(WorldUnloadEvent e) {
-        System.out.println(ChatColor.RED+"SLIMEFUN- Unloading world ... "+e.getWorld().getName());
         BlockStorage storage = BlockStorage.getStorage(e.getWorld());
 
         if (storage != null) {
