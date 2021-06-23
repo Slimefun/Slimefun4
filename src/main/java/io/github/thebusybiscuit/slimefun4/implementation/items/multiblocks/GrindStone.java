037747983ccbf4dd71d8e9f21a98539772ad7973
svr333 <vanrompaey.senne@protonmail.com>
2021-06-23 10:26:29 +0200

    Update src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/GrindStone.java
    Co-authored-by: Semisol <git@smileycreations15.com>

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/GrindStone.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/multiblocks/GrindStone.java
@@ -91,10 +91,6 @@ public class GrindStone extends MultiBlockMachine {
 
         recipes.add(SlimefunItems.ENDER_LUMP_3);
         recipes.add(new SlimefunItemStack(SlimefunItems.ENDER_LUMP_2, 4));
-
-        recipes.add(SlimefunItems.CARBON);
-        recipes.add(new ItemStack(Material.COAL, 8));
-
         recipes.add(new ItemStack(Material.DIAMOND));
         recipes.add(new SlimefunItemStack(SlimefunItems.CARBON, 4));
     }
