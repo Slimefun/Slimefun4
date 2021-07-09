8a99ee409e773c3f07c521a52cf0a6ac0c078b4c
StarWishsama <starwishsama@outlook.com>
2021-07-09 22:57:16 +0800

    fix(codestyle): use space

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/menu/AndroidShareMenu.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/androids/menu/AndroidShareMenu.java
@@ -32,206 +32,207 @@ import java.util.UUID;
  * @see ProgrammableAndroid
  */
 public final class AndroidShareMenu {
-	private static final int DISPLAY_START_SLOT = 9;
-	private static final int DISPLAY_END_SLOT = 45;
-	private static final String BLOCK_INFO_KEY = "share-users";
+  private static final int DISPLAY_START_SLOT = 9;
+  private static final int DISPLAY_END_SLOT = 45;
+  private static final String BLOCK_INFO_KEY = "share-users";
 
-	private AndroidShareMenu() {}
+  private AndroidShareMenu() {}
 
-	/**
-	 * Open a share menu for player.
-	 *
-	 * @param p player
-	 * @param b android
-	 * @param page page
-	 */
-	@ParametersAreNonnullByDefault
-	public static void openShareMenu(Player p, Block b, int page) {
-		Validate.notNull(p, "The player cannot be null!");
-		Validate.notNull(b, "The android block cannot be null!");
-		Validate.isTrue(page > 0, "The page must be above or equals 0!");
+  /**
+   * Open a share menu for player.
+   *
+   * @param p player
+   * @param b android
+   * @param page page
+   */
+  @ParametersAreNonnullByDefault
+  public static void openShareMenu(Player p, Block b, int page) {
+    Validate.notNull(p, "The player cannot be null!");
+    Validate.notNull(b, "The android block cannot be null!");
+    Validate.isTrue(page > 0, "The page must be above or equals 0!");
 
-		ChestMenu menu = new ChestMenu(SlimefunPlugin.instance(), SlimefunPlugin.getLocalization().getMessage("android.access-manager.title"));
+    ChestMenu menu = new ChestMenu(SlimefunPlugin.instance(), SlimefunPlugin.getLocalization().getMessage("android.access-manager.title"));
 
-		menu.setEmptySlotsClickable(false);
+    menu.setEmptySlotsClickable(false);
 
-		List<String> users = getTrustedUsers(b);
+    List<String> users = getTrustedUsers(b);
 
-		int pages = Math.max(1, users.size() / 36);
+    int pages = Math.max(1, users.size() / 36);
 
-		// Draw background start
-		for (int i = 0; i < 9; i++) {
-			menu.addItem(i, ChestMenuUtils.getBackground());
-			menu.addMenuClickHandler(i, (pl, slot, item, cursor, action) -> false);
-		}
+    // Draw background start
+    for (int i = 0; i < 9; i++) {
+      menu.addItem(i, ChestMenuUtils.getBackground());
+      menu.addMenuClickHandler(i, (pl, slot, item, cursor, action) -> false);
+    }
 
-		for (int i = 45; i < 54; i++) {
-			menu.addItem(i, ChestMenuUtils.getBackground());
-			menu.addMenuClickHandler(i, (pl, slot, item, cursor, action) -> false);
-		}
-		// Draw background end
+    for (int i = 45; i < 54; i++) {
+      menu.addItem(i, ChestMenuUtils.getBackground());
+      menu.addMenuClickHandler(i, (pl, slot, item, cursor, action) -> false);
+    }
+    // Draw background end
 
-		// Add trusted player slot
-		menu.addItem(0, new CustomItem(HeadTexture.SCRIPT_UP.getAsItemStack(), SlimefunPlugin.getLocalization().getMessage("android.access-manager.menu.add-player-title"), SlimefunPlugin.getLocalization().getMessage("android.access-manager.menu.add-player")));
-		menu.addMenuClickHandler(0, (p1, slot, item, cursor, action) -> {
-			p1.closeInventory();
+    // Add trusted player slot
+    menu.addItem(0, new CustomItem(HeadTexture.SCRIPT_UP.getAsItemStack(), SlimefunPlugin.getLocalization().getMessage("android.access-manager.menu.add-player-title"), SlimefunPlugin.getLocalization().getMessage("android.access-manager.menu.add-player")));
+    menu.addMenuClickHandler(0, (p1, slot, item, cursor, action) -> {
+          p1.closeInventory();
 
-			SlimefunPlugin.getLocalization().sendMessage(p1, "android.access-manager.messages.input");
+          SlimefunPlugin.getLocalization().sendMessage(p1, "android.access-manager.messages.input");
 
-			ChatUtils.awaitInput(p1, message -> {
-				Player target = Bukkit.getPlayerExact(message);
+          ChatUtils.awaitInput(p1, message -> {
+              Player target = Bukkit.getPlayerExact(message);
 
-				if (target == null) {
-					SlimefunPlugin.getLocalization().sendMessage(p1, "android.access-manager.messages.cannot-find-player", msg -> msg.replace("%player%", p1.getName()));
-				} else {
-					addPlayer(p1, target, b, users);
-				}
-			});
-			return false;
-		});
+              if (target == null) {
+                  SlimefunPlugin.getLocalization().sendMessage(p1, "android.access-manager.messages.cannot-find-player", msg -> msg.replace("%player%", p1.getName()));
+              } else {
+                  addPlayer(p1, target, b, users);
+              }
+          });
+          return false;
+        });
 
-		// Display added trusted player(s)
-		if (!users.isEmpty()) {
-			List<String> displayUsers = users.subList(page, Math.min(users.size() - 1, DISPLAY_END_SLOT));
+    // Display added trusted player(s)
+    if (!users.isEmpty()) {
+      List<String> displayUsers = users.subList(page, Math.min(users.size() - 1, DISPLAY_END_SLOT));
 
-			for (int index = 0; index < displayUsers.size(); index++) {
-				int slot = index + DISPLAY_START_SLOT;
-				OfflinePlayer current = Bukkit.getOfflinePlayer(UUID.fromString(displayUsers.get(index)));
-				menu.addItem(slot, new CustomItem(SkullItem.fromPlayer(current), "&b" + current.getName(), SlimefunPlugin.getLocalization().getMessage("android.access-manager.menu.delete-player")));
-				menu.addMenuClickHandler(slot, (p1, slot1, item, cursor, action) -> {
-					if (action.isLeftClick()) {
-						removePlayer(p1, current, b, users);
-					}
+      for (int index = 0; index < displayUsers.size(); index++) {
+        int slot = index + DISPLAY_START_SLOT;
+        OfflinePlayer current = Bukkit.getOfflinePlayer(UUID.fromString(displayUsers.get(index)));
+        menu.addItem(slot, new CustomItem(SkullItem.fromPlayer(current), "&b" + current.getName(), SlimefunPlugin.getLocalization().getMessage("android.access-manager.menu.delete-player")));
+        menu.addMenuClickHandler(slot, (p1, slot1, item, cursor, action) -> {
+            if (action.isLeftClick()) {
+                removePlayer(p1, current, b, users);
+            }
+            return false;
+        });
+      }
+    }
 
-					return false;
-				});
-			}
-		}
+    if (pages > 0) {
+      menu.addItem(47, ChestMenuUtils.getPreviousButton(p, page, pages));
+      menu.addMenuClickHandler(46, (pl, slot, item, cursor, action) -> {
+          int previousPage = page - 1;
+          if (previousPage < 1) {
+              previousPage = pages;
+          }
 
-		if (pages > 0) {
-			menu.addItem(47, ChestMenuUtils.getPreviousButton(p, page, pages));
-			menu.addMenuClickHandler(46, (pl, slot, item, cursor, action) -> {
-				int previousPage = page - 1;
-				if (previousPage < 1) {
-					previousPage = pages;
-				}
+          if (previousPage != page) {
+              openShareMenu(p, b, previousPage);
+          }
+          return false;
+      });
 
-				if (previousPage != page) {
-					openShareMenu(p, b, previousPage);
-				}
-				return false;
-			});
+      menu.addItem(51, ChestMenuUtils.getNextButton(p, page, pages));
+      menu.addMenuClickHandler(
+          50,
+          (pl, slot, item, cursor, action) -> {
+            int nextPage = page + 1;
 
-			menu.addItem(51, ChestMenuUtils.getNextButton(p, page, pages));
-			menu.addMenuClickHandler(50, (pl, slot, item, cursor, action) -> {
-				int nextPage = page + 1;
+            if (nextPage > pages) {
+              nextPage = 1;
+            }
 
-				if (nextPage > pages) {
-					nextPage = 1;
-				}
+            if (nextPage != page) {
+              openShareMenu(p, b, nextPage);
+            }
+            return false;
+          });
+    }
 
-				if (nextPage != page) {
-					openShareMenu(p, b, nextPage);
-				}
-				return false;
-			});
-		}
+    menu.open(p);
+  }
 
-		menu.open(p);
-	}
+  @ParametersAreNonnullByDefault
+  private static void addPlayer(Player owner, OfflinePlayer p, Block android, List<String> users) {
+    Validate.notNull(owner, "The android cannot be null!");
+    Validate.notNull(p, "The target player cannot be null!");
+    Validate.notNull(android, "The android block cannot be null!");
+    Validate.notNull(users, "The trusted users list cannot be null!");
 
-	@ParametersAreNonnullByDefault
-	private static void addPlayer(Player owner, OfflinePlayer p, Block android, List<String> users) {
-		Validate.notNull(owner, "The android cannot be null!");
-		Validate.notNull(p, "The target player cannot be null!");
-		Validate.notNull(android, "The android block cannot be null!");
-		Validate.notNull(users, "The trusted users list cannot be null!");
+    if (users.contains(p.getUniqueId().toString())) {
+      SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.is-trusted-player", msg -> msg.replace("%player%", p.getName()));
+    } else if (owner.getUniqueId() == p.getUniqueId()) {
+      SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.cannot-add-yourself");
+    } else {
+      users.add(p.getUniqueId().toString());
+      SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.add-success", msg -> msg.replace("%player%", p.getName()));
 
-		if (users.contains(p.getUniqueId().toString())) {
-			SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.is-trusted-player", msg -> msg.replace("%player%", p.getName()));
-		} else if (owner.getUniqueId() == p.getUniqueId()) {
-			SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.cannot-add-yourself");
-		} else {
-			users.add(p.getUniqueId().toString());
-			SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.add-success", msg -> msg.replace("%player%", p.getName()));
+      BlockStorage.addBlockInfo(android, BLOCK_INFO_KEY, users.toString());
+    }
+  }
 
-			BlockStorage.addBlockInfo(android, BLOCK_INFO_KEY, users.toString());
-		}
-	}
+  @ParametersAreNonnullByDefault
+  private static void removePlayer(Player owner, OfflinePlayer p, Block android, List<String> users) {
+    Validate.notNull(owner, "The android cannot be null!");
+    Validate.notNull(p, "The target player cannot be null!");
+    Validate.notNull(android, "The android block cannot be null!");
+    Validate.notNull(users, "The trusted users list cannot be null!");
 
-	@ParametersAreNonnullByDefault
-	private static void removePlayer(Player owner, OfflinePlayer p, Block android, List<String> users) {
-		Validate.notNull(owner, "The android cannot be null!");
-		Validate.notNull(p, "The target player cannot be null!");
-		Validate.notNull(android, "The android block cannot be null!");
-		Validate.notNull(users, "The trusted users list cannot be null!");
+    if (users.contains(p.getUniqueId().toString())) {
+      users.remove(p.getUniqueId().toString());
+      SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.delete-success", msg -> msg.replace("%player%", p.getName()));
 
-		if (users.contains(p.getUniqueId().toString())) {
-			users.remove(p.getUniqueId().toString());
-			SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.delete-success", msg -> msg.replace("%player%", p.getName()));
+      BlockStorage.addBlockInfo(android, BLOCK_INFO_KEY, users.toString());
+    } else {
+      SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.is-not-trusted-player", msg -> msg.replace("%player%", p.getName()));
+    }
+  }
 
-			BlockStorage.addBlockInfo(android, BLOCK_INFO_KEY, users.toString());
-		} else {
-			SlimefunPlugin.getLocalization().sendMessage(owner, "android.access-manager.messages.is-not-trusted-player", msg -> msg.replace("%player%", p.getName()));
-		}
-	}
+  /**
+   * Parse trusted player list raw string to List.
+   *
+   * @param value list raw string
+   * @return trusted player list
+   */
+  private @Nonnull static List<String> parseBlockInfoToList(@Nonnull String value) {
+    Validate.notNull(value, "The trusted player list cannot be null!");
 
-	/**
-	 * Parse trusted player list raw string to List.
-	 *
-	 * @param value list raw string
-	 * @return trusted player list
-	 */
-	private @Nonnull static List<String> parseBlockInfoToList(@Nonnull String value) {
-		Validate.notNull(value, "The trusted player list cannot be null!");
+    String replacedText = value.replace("[", "").replace("]", "");
 
-		String replacedText = value.replace("[", "").replace("]", "");
+    if (replacedText.isEmpty()) {
+      return Collections.emptyList();
+    } else {
+      return Arrays.asList(replacedText.split(", "));
+    }
+  }
 
-		if (replacedText.isEmpty()) {
-			return Collections.emptyList();
-		} else {
-			return Arrays.asList(replacedText.split(", "));
-		}
-	}
+  /**
+   * Get a trusted users list of specific android.
+   *
+   * @param b the block of a Android
+   * @return Trusted users
+   */
+  public @Nonnull static List<String> getTrustedUsers(@Nonnull Block b) {
+    Validate.notNull(b, "The android block cannot be null!");
 
-	/**
-	 * Get a trusted users list of specific android.
-	 *
-	 * @param b the block of a Android
-	 * @return Trusted users
-	 */
-	public @Nonnull static List<String> getTrustedUsers(@Nonnull Block b) {
-		Validate.notNull(b, "The android block cannot be null!");
+    String list = BlockStorage.getLocationInfo(b.getLocation(), BLOCK_INFO_KEY);
 
-		String list = BlockStorage.getLocationInfo(b.getLocation(), BLOCK_INFO_KEY);
+    // Checks for old Android
+    if (list == null) {
+      BlockStorage.addBlockInfo(b, "share-users", Collections.emptyList().toString());
+      return Collections.emptyList();
+    }
 
-		// Checks for old Android
-		if (list == null) {
-			BlockStorage.addBlockInfo(b, "share-users", Collections.emptyList().toString());
-			return Collections.emptyList();
-		}
+    return parseBlockInfoToList(list);
+  }
 
-		return parseBlockInfoToList(list);
-	}
+  /**
+   * Checks user is in trusted users list.
+   *
+   * @param b the block of a Android
+   * @param uuid user's UUID
+   * @return whether is the trusted user of android or not
+   */
+  @ParametersAreNonnullByDefault
+  public static boolean isTrustedUser(Block b, UUID uuid) {
+    Validate.notNull(b, "The android block cannot be null!");
+    Validate.notNull(uuid, "The UUID of player to check cannot be null!");
 
-	/**
-	 * Checks user is in trusted users list.
-	 *
-	 * @param b the block of a Android
-	 * @param uuid user's UUID
-	 * @return whether is the trusted user of android or not
-	 */
-	@ParametersAreNonnullByDefault
-	public static boolean isTrustedUser(Block b, UUID uuid) {
-		Validate.notNull(b, "The android block cannot be null!");
-		Validate.notNull(uuid, "The UUID of player to check cannot be null!");
+    String trustUsers = BlockStorage.getLocationInfo(b.getLocation(), BLOCK_INFO_KEY);
 
-		String trustUsers = BlockStorage.getLocationInfo(b.getLocation(), BLOCK_INFO_KEY);
+    if (trustUsers == null) {
+      return false;
+    }
 
-		if (trustUsers == null) {
-			return false;
-		}
-
-		return trustUsers.contains(uuid.toString());
-	}
+    return trustUsers.contains(uuid.toString());
+  }
 }
