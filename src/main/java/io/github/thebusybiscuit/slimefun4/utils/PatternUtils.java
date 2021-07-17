a2a42a7b62d63abc2e1ed92582607fa26e07dcab
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-07-17 19:19:40 +0200

    Fixed PatternUtils

--- src/main/java/io/github/thebusybiscuit/slimefun4/utils/PatternUtils.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/utils/PatternUtils.java
@@ -2,7 +2,7 @@ package io.github.thebusybiscuit.slimefun4.utils;
 
 import java.util.regex.Pattern;
 
-import io.github.thebusybiscuit.cscorelib2.chat.ChatColors;
+import net.md_5.bungee.api.ChatColor;
 
 /**
  * This class is created for common-use patterns used in things such as {@link String#split(String)}. <br>
@@ -35,6 +35,6 @@ public final class PatternUtils {
     public static final Pattern MINECRAFT_TAG = Pattern.compile("#minecraft:[a-z_]+");
     public static final Pattern SLIMEFUN_TAG = Pattern.compile("#slimefun:[a-z_]+");
 
-    public static final Pattern USES_LEFT_LORE = Pattern.compile(ChatColors.color("&e[0-9]+ Uses? &7left"));
+    public static final Pattern USES_LEFT_LORE = Pattern.compile(ChatColor.YELLOW + "[0-9]+ Uses? " + ChatColor.GRAY + "left");
 
 }
