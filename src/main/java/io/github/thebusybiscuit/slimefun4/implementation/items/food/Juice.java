82f3662c91996605758b99867f88afba0d9d54fa
qwertyuioplkjhgfd <charles07023@gmail.com>
2021-09-29 18:43:57 -0700

    fix

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/food/Juice.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/food/Juice.java
@@ -6,8 +6,6 @@ import java.util.List;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
@@ -19,11 +17,9 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemConsumptionHandler;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
 import io.github.thebusybiscuit.slimefun4.implementation.items.backpacks.Cooler;
 import io.github.thebusybiscuit.slimefun4.implementation.listeners.CoolerListener;
-import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 
 /**
  * This class represents a {@link SlimefunItem} that can be stored inside
@@ -71,33 +67,8 @@ public class Juice extends SimpleSlimefunItem<ItemConsumptionHandler> {
                 }
             }
 
-            removeGlassBottle(p, item);
+            e.setItem(null);
         };
-    }
-
-    /**
-     * Determines from which hand the juice is being drunk, and its amount
-     * 
-     * @param p
-     *            The {@link Player} that triggered this
-     * @param item
-     *            The {@link ItemStack} in question
-     */
-    @ParametersAreNonnullByDefault
-    private void removeGlassBottle(Player p, ItemStack item) {
-        if (SlimefunUtils.isItemSimilar(item, p.getInventory().getItemInMainHand(), true)) {
-            if (p.getInventory().getItemInMainHand().getAmount() == 1) {
-                Slimefun.runSync(() -> p.getEquipment().getItemInMainHand().setAmount(0));
-            } else {
-                Slimefun.runSync(() -> p.getInventory().removeItem(new ItemStack(Material.GLASS_BOTTLE, 1)));
-            }
-        } else if (SlimefunUtils.isItemSimilar(item, p.getInventory().getItemInOffHand(), true)) {
-            if (p.getInventory().getItemInOffHand().getAmount() == 1) {
-                Slimefun.runSync(() -> p.getEquipment().getItemInOffHand().setAmount(0));
-            } else {
-                Slimefun.runSync(() -> p.getInventory().removeItem(new ItemStack(Material.GLASS_BOTTLE, 1)));
-            }
-        }
     }
 
 }
