fcc2bf9bde07449ae78e2478cdd82b2feaabf4bf
FluffyBear <31554056+ncbpfluffybear@users.noreply.github.com>
2021-11-04 20:07:05 -0500

    block custom item insertion

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/CargoNodeListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/CargoNodeListener.java
@@ -1,11 +1,15 @@
 package io.github.thebusybiscuit.slimefun4.implementation.listeners;
 
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import javax.annotation.Nonnull;
 
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
@@ -32,6 +36,28 @@ public class CargoNodeListener implements Listener {
         if ((b.getY() != e.getBlockAgainst().getY() || !e.getBlockReplacedState().getType().isAir()) && isCargoNode(e.getItemInHand())) {
             Slimefun.getLocalization().sendMessage(e.getPlayer(), "machines.CARGO_NODES.must-be-placed", true);
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onFilterMove(InventoryClickEvent e) {
+        String invName = e.getWhoClicked().getOpenInventory().getTitle();
+        if ((invName.equals("Cargo Node (Input)") || invName.equals("Advanced Cargo Node (Output)"))) {
+            if (e.getClick() == ClickType.NUMBER_KEY) {
+                e.setCancelled(true);
+                return;
+            }
+
+            ItemStack item = e.getCurrentItem();
+            if (item == null) {
+                return;
+            }
+
+            SlimefunItem sfItem = SlimefunItem.getByItem(item);
+            if (sfItem == null && item.getItemMeta() != new ItemStack(item.getType()).getItemMeta()) {
+                Slimefun.getLocalization().sendMessage(e.getWhoClicked(), "machines.CARGO_NODES.no-custom-items", true);
+                e.setCancelled(true);
+            }
         }
     }
 
