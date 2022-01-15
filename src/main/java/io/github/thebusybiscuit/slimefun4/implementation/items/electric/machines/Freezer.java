4f2900045ab1e29c1f3c1c0e0ab2e9e59eae2fc4
J3fftw <44972470+J3fftw1@users.noreply.github.com>
2022-01-15 22:29:22 +0100

    Update src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/Freezer.java
    Co-authored-by: Daniel Walsh <walshydev@gmail.com>

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/Freezer.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/Freezer.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Material;
-
 import org.bukkit.inventory.ItemStack;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
