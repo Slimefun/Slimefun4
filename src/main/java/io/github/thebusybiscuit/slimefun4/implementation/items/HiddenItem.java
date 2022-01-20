6f5f5e585fe5a47bb1e894da87af682c73013b6f
qwertyuioplkjhgfd <45241413+qwertyuioplkjhgfd@users.noreply.github.com>
2022-01-20 04:15:38 +0000

    Update src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/HiddenItem.java
    Co-authored-by: poma123 <25465545+poma123@users.noreply.github.com>

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/HiddenItem.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/HiddenItem.java
@@ -1,14 +1,14 @@
 package io.github.thebusybiscuit.slimefun4.implementation.items;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import org.bukkit.inventory.ItemStack;
+
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.NotConfigurable;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-
-import org.bukkit.inventory.ItemStack;
 
 /**
  * The {@link HiddenItem} is a {@link NotConfigurable} {@link SlimefunItem}
