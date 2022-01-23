392d6236dc6271c9d8499769f7087e8f05861f34
StarWishsama <starwishsama@outlook.com>
2022-01-23 22:33:24 +0800

    refactor(pedestal): add timeout for future

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/Slimefun.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/Slimefun.java
@@ -353,7 +353,7 @@ public final class Slimefun extends JavaPlugin implements SlimefunAddon {
         }
 
         // Pedestal item watcher Task
-        getServer().getScheduler().runTaskTimerAsynchronously(this, new AncientPedestalTask((AncientPedestal) SlimefunItems.ANCIENT_PEDESTAL.getItem()), 5 * 20L, 5 * 20L);
+        new AncientPedestalTask(this, (AncientPedestal) SlimefunItems.ANCIENT_PEDESTAL.getItem());
 
         // Starting our tasks
         autoSavingService.start(this, config.getInt("options.auto-save-delay-in-minutes"));
--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/tasks/AncientPedestalTask.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/tasks/AncientPedestalTask.java
@@ -3,20 +3,21 @@ package io.github.thebusybiscuit.slimefun4.implementation.tasks;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import javax.annotation.Nonnull;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-
+import org.bukkit.scheduler.BukkitScheduler;
 
 import io.github.bakedlibs.dough.blocks.BlockPosition;
 import io.github.thebusybiscuit.slimefun4.implementation.items.altar.AncientPedestal;
 import io.github.thebusybiscuit.slimefun4.implementation.listeners.AncientAltarListener;
-
-import javax.annotation.Nonnull;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 
 /**
  * The {@link AncientPedestalTask} is responsible for checking item
@@ -32,9 +33,15 @@ import javax.annotation.Nonnull;
 public class AncientPedestalTask implements Runnable {
 
     private final Map<BlockPosition, UUID> virtualItemCache;
+    private final Slimefun plugin;
+    private final BukkitScheduler scheduler;
 
-    public AncientPedestalTask(@Nonnull AncientPedestal pedestalItem) {
+    public AncientPedestalTask(@Nonnull Slimefun plugin, @Nonnull AncientPedestal pedestalItem) {
+        this.plugin = plugin;
+        scheduler = plugin.getServer().getScheduler();
         this.virtualItemCache = pedestalItem.getVirtualItemCache();
+
+        scheduler.runTaskTimerAsynchronously(plugin, this, 5 * 20L, 5 * 20L);
     }
 
     @Override
@@ -51,7 +58,7 @@ public class AncientPedestalTask implements Runnable {
             if (itemUUID != null) {
                 // Avoid async catcher
                 try {
-                    Entity displayItem = Slimefun.instance().getServer().getScheduler().callSyncMethod(Slimefun.instance(), () -> Bukkit.getEntity(itemUUID)).get();
+                    Entity displayItem = scheduler.callSyncMethod(plugin, () -> Bukkit.getEntity(itemUUID)).get(3, TimeUnit.SECONDS);
                     if (displayItem != null && displayItem.getLocation().distanceSquared(spawnLocation) > 1) {
                         if (displayItem.isValid()) {
                             displayItem.teleport(spawnLocation);
@@ -59,7 +66,7 @@ public class AncientPedestalTask implements Runnable {
                             virtualItemCache.remove(blockPosition);
                         }
                     }
-                } catch (InterruptedException | ExecutionException x) {
+                } catch (InterruptedException | ExecutionException | TimeoutException x) {
                     Slimefun.logger().log(Level.WARNING, x, () -> "An Error occurred while processing pedestal item!");
                 }
             }
