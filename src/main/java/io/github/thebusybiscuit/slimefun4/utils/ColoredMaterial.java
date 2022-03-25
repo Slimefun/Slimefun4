d68691a057c7cb17cefcb71ecc76c47db81e9e31
haiman <83174104+haiman233@users.noreply.github.com>
2022-03-25 12:03:04 +0800

    The rainbow bed :p

--- src/main/java/io/github/thebusybiscuit/slimefun4/utils/ColoredMaterial.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/utils/ColoredMaterial.java
@@ -221,6 +221,28 @@ public enum ColoredMaterial {
             Material.GREEN_CANDLE,
             Material.RED_CANDLE,
             Material.BLACK_CANDLE
+    }),
+    
+        /**
+     * This {@link List} contains all bed colors ordered by their appearance ingame.
+     */
+    BED(new Material[] {
+            Material.WHITE_BED,
+            Material.ORANGE_BED,
+            Material.MAGENTA_BED,
+            Material.LIGHT_BLUE_BED,
+            Material.YELLOW_BED,
+            Material.LIME_BED,
+            Material.PINK_BED,
+            Material.GRAY_BED,
+            Material.LIGHT_GRAY_BED,
+            Material.CYAN_BED,
+            Material.PURPLE_BED,
+            Material.BLUE_BED,
+            Material.BROWN_BED,
+            Material.GREEN_BED,
+            Material.RED_BED,
+            Material.BLACK_BED
     });
     
     // @formatter:on
