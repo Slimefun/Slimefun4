ddb2e3f920f252525efa33024f1619ee87053d98
terslenk <terslenkhd@gmail.com>
2022-07-06 11:59:53 +0300

    final touch

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/tools/SmeltersTool.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/tools/SmeltersTool.java
@@ -1,13 +1,11 @@
 package io.github.thebusybiscuit.slimefun4.implementation.items.tools;
 
-import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.DamageableItem;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ToolUseHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
 import io.github.thebusybiscuit.slimefun4.utils.tags.SlimefunTag;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
@@ -18,9 +16,7 @@ import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 public class SmeltersTool extends SimpleSlimefunItem<ToolUseHandler> implements DamageableItem {
@@ -36,31 +32,7 @@ public class SmeltersTool extends SimpleSlimefunItem<ToolUseHandler> implements 
         return (e, tool, fortune, drops) -> {
             Block b = e.getBlock();
 
-            if (SlimefunTag.PICKAXES.isTagged(tool.getType()) && SlimefunTag.SMELTERS_PICKAXE_BLOCKS.isTagged(b.getType()) && !BlockStorage.hasBlockInfo(b)) {
-                Collection<ItemStack> blockDrops = b.getDrops(tool);
-
-                for (ItemStack drop : blockDrops) {
-                    if (drop != null && !drop.getType().isAir()) {
-                        smelt(b, drop, fortune);
-                        drops.add(drop);
-                    }
-                }
-
-                damageItem(e.getPlayer(), tool);
-            }
-            if (SlimefunTag.SHOVELS.isTagged(tool.getType()) && SlimefunTag.SMELTERS_SHOVEL_BLOCKS.isTagged(b.getType()) && !BlockStorage.hasBlockInfo(b)) {
-                Collection<ItemStack> blockDrops = b.getDrops(tool);
-
-                for (ItemStack drop : blockDrops) {
-                    if (drop != null && !drop.getType().isAir()) {
-                        smelt(b, drop, fortune);
-                        drops.add(drop);
-                    }
-                }
-
-                damageItem(e.getPlayer(), tool);
-            }
-            if (SlimefunTag.AXES.isTagged(tool.getType()) && SlimefunTag.SMELTERS_AXE_BLOCKS.isTagged(b.getType()) && !BlockStorage.hasBlockInfo(b)) {
+            if (isValid(tool, b)) {
                 Collection<ItemStack> blockDrops = b.getDrops(tool);
 
                 for (ItemStack drop : blockDrops) {
@@ -73,6 +45,24 @@ public class SmeltersTool extends SimpleSlimefunItem<ToolUseHandler> implements 
                 damageItem(e.getPlayer(), tool);
             }
         };
+    }
+
+    private boolean isValid(@Nonnull ItemStack tool, @Nonnull Block b) {
+        if (BlockStorage.hasBlockInfo(b)) {
+            return false;
+        } else {
+            Material toolMaterial = tool.getType();
+            Material blockMaterial = b.getType();
+            if (SlimefunTag.PICKAXES.isTagged(toolMaterial) && SlimefunTag.SMELTERS_PICKAXE_BLOCKS.isTagged(blockMaterial)) {
+                return true;
+            } else if (SlimefunTag.SHOVELS.isTagged(toolMaterial) && SlimefunTag.SMELTERS_SHOVEL_BLOCKS.isTagged(blockMaterial)) {
+                return true;
+            } else if (SlimefunTag.AXES.isTagged(toolMaterial) && SlimefunTag.SMELTERS_AXE_BLOCKS.isTagged(blockMaterial)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     @ParametersAreNonnullByDefault
