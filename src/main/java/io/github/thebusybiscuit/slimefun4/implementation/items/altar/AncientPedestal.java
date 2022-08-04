6e2ba8ce0c1464e8f84a372f13b646081300734a
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2022-08-04 03:43:23 -0500

    Pet Peeve
    Just A Pet Peeve

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/altar/AncientPedestal.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/altar/AncientPedestal.java
@@ -24,6 +24,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
+
 import io.github.bakedlibs.dough.common.ChatColors;
 import io.github.bakedlibs.dough.items.CustomItemStack;
 import io.github.bakedlibs.dough.items.ItemUtils;
