317a92bd69a9f7429da333b64e84cfee1428ca9c
Jeffrey Kosse <j3fftw@gmail.com>
2022-09-30 20:54:33 +0200

    fix 3577

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/BackpackListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/BackpackListener.java
@@ -155,6 +155,10 @@ public class BackpackListener implements Listener {
             p.closeInventory();
         }
 
+        if (p.getInventory().getItemInMainHand() == item) {
+            return;
+        }
+
         // Check if someone else is currently viewing this backpack
         if (!backpacks.containsValue(item)) {
             p.playSound(p.getLocation(), Sound.ENTITY_HORSE_ARMOR, 1F, 1F);
