937b1d9b42d1a5637bdaef6edb665b81d65971bf
zeyad rashed <brwnboy2002@gmail.com>
2022-12-20 01:14:01 -0500

    lowered # of waypoints 21 -> 5

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/player/PlayerProfile.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/player/PlayerProfile.java
@@ -1,35 +1,7 @@
 package io.github.thebusybiscuit.slimefun4.api.player;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.Set;
-import java.util.UUID;
-import java.util.function.Consumer;
-import java.util.logging.Level;
-import java.util.stream.IntStream;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import org.apache.commons.lang.Validate;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-
 import io.github.bakedlibs.dough.common.ChatColors;
 import io.github.bakedlibs.dough.common.CommonPatterns;
 import io.github.bakedlibs.dough.config.Config;
@@ -43,6 +15,18 @@ import io.github.thebusybiscuit.slimefun4.core.guide.GuideHistory;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.items.armor.SlimefunArmorPiece;
 import io.github.thebusybiscuit.slimefun4.utils.NumberUtils;
+import org.apache.commons.lang.Validate;
+import org.bukkit.*;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.stream.IntStream;
 
 /**
  * A class that can store a Player's {@link Research} progress for caching purposes.
@@ -256,7 +240,7 @@ public class PlayerProfile {
             }
         }
 
-        if (waypoints.size() < 21) {
+        if (waypoints.size() < 5) { //i dont actually know how much you want cuz i never played this server and have no clue how to balance it but i just wanted to help and b included -zune
             waypoints.add(waypoint);
 
             waypointsFile.setValue(waypoint.getId(), waypoint.getLocation());
