de95e684e793bd7101aebfa37e632832fe885b01
J3fftw1 <jeffrey@kosse.me>
2023-03-21 15:28:08 +0100

    fix error on startup

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
@@ -64,6 +64,7 @@ class ContributionsConnector extends GitHubConnector {
         ignoredAccounts.add("gitlocalize-app");
         ignoredAccounts.add("gitlocalize-app[bot]");
         ignoredAccounts.add("mt-gitlocalize");
+        ignoredAccounts.add("amarcais53");
 
         // Known Minecraft aliases.
         aliases.put("WalshyDev", "HumanRightsAct");
