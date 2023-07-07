4e35f62e0e159125f055b2d6bc2f24a242d33c16
Sefiraat <miakis@me.com>
2023-07-07 18:55:35 +0100

    Adding ExternallyInteractable (#3788)
    Co-authored-by: Daniel Walsh <walshydev@gmail.com>

---
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/attributes/interactions/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the various possible {@link io.github.thebusybiscuit.slimefun4.core.attributes.interactions.InteractionResult}s
+ * that can be returned by an {@link io.github.thebusybiscuit.slimefun4.core.attributes.ExternallyInteractable} object.
+ */
+package io.github.thebusybiscuit.slimefun4.core.attributes.interactions;
