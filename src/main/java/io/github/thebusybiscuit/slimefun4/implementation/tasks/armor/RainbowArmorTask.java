5ef2296f6f6c8dc11da9be0c861d429ff965cbf1
iTwins <teriele.jeroen@gmail.com>
2023-08-09 20:30:57 +0200

    optimized to use HashedArmorpiece instead of getByItem

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/tasks/armor/RainbowArmorTask.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/tasks/armor/RainbowArmorTask.java
@@ -1,5 +1,7 @@
 package io.github.thebusybiscuit.slimefun4.implementation.tasks.armor;
 
+import java.util.Optional;
+
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import org.bukkit.Color;
@@ -7,9 +9,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.HashedArmorpiece;
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.implementation.items.armor.RainbowArmorPiece;
+import io.github.thebusybiscuit.slimefun4.implementation.items.armor.SlimefunArmorPiece;
 
 /**
  * The {@link RainbowArmorTask} is responsible for handling the change in color of any Rainbow Armor piece.
@@ -28,12 +31,12 @@ public class RainbowArmorTask extends AbstractArmorTask {
     @Override
     @ParametersAreNonnullByDefault
     protected void onPlayerTick(Player p, PlayerProfile profile) {
+        final ItemStack[] armorContents = p.getInventory().getArmorContents();
+        final HashedArmorpiece[] hashedArmorpieces = profile.getArmor();
         for (int i = 0; i < 4; i++) {
-            ItemStack item = p.getInventory().getArmorContents()[i];
-            SlimefunItem sfItem = SlimefunItem.getByItem(item);
-
-            if (sfItem instanceof RainbowArmorPiece rainbowArmorPiece && rainbowArmorPiece.canUse(p, true)) {
-                updateRainbowArmor(item, rainbowArmorPiece);
+            Optional<SlimefunArmorPiece> sfArmorPiece = hashedArmorpieces[i].getItem();
+            if (sfArmorPiece.isPresent() && sfArmorPiece.get() instanceof RainbowArmorPiece rainbowArmorPiece && rainbowArmorPiece.canUse(p, true) && armorContents[i].hasItemMeta() && !hashedArmorpieces[i].hasDiverged(armorContents[i], true)) {
+                updateRainbowArmor(armorContents[i], rainbowArmorPiece);
             }
         }
     }
