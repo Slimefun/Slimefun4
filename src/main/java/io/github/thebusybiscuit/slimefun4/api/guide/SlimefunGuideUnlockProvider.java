fccad03da1feb010652a9fce04d3f0d3de1b3030
StarWishsama <starwishsama@outlook.com>
2023-08-14 10:07:32 +0800

    feat(guide): support custom locked item

--- src/main/java/io/github/thebusybiscuit/slimefun4/api/guide/SlimefunGuideUnlockProvider.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/api/guide/SlimefunGuideUnlockProvider.java
@@ -1,8 +1,15 @@
 package io.github.thebusybiscuit.slimefun4.api.guide;
 
+import io.github.bakedlibs.dough.items.CustomItemStack;
+import io.github.bakedlibs.dough.items.ItemUtils;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.researches.Research;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import javax.annotation.Nonnull;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * The {@link SlimefunGuideUnlockProvider} used for process unlock research
@@ -39,4 +46,15 @@ public interface SlimefunGuideUnlockProvider {
      * @return unit name
      */
     @Nonnull String getUnitName();
+
+    /**
+     * This returns guide item when research locked
+     * By default, it shows up in the guide as a barrier with a name & the experience cost in it.
+     *
+     * @return locked item {@link ItemStack}
+     */
+    @Nonnull
+    default ItemStack getLockedItem(@Nonnull Research research, @Nonnull SlimefunItem sfItem, @Nonnull Player p) {
+        return new CustomItemStack(ChestMenuUtils.getNotResearchedItem(), ChatColor.WHITE + ItemUtils.getItemName(sfItem.getItem()), "&4&l" + Slimefun.getLocalization().getMessage(p, "guide.locked"), "", "&a> Click to unlock", "", "&7Cost: &b" + research.getCost() + " " + getUnitName());
+    }
 }
