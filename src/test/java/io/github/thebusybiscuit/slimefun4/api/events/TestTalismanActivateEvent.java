012da241b3f1456271959b0da3a70190367aabcc
Alessio Colombo <37039432+Sfiguz7@users.noreply.github.com>
2023-12-12 21:31:44 +0100

    Update src/test/java/io/github/thebusybiscuit/slimefun4/api/events/TestTalismanActivateEvent.java

--- src/test/java/io/github/thebusybiscuit/slimefun4/api/events/TestTalismanActivateEvent.java
+++ src/test/java/io/github/thebusybiscuit/slimefun4/api/events/TestTalismanActivateEvent.java
@@ -140,7 +140,7 @@ class TestTalismanActivateEvent {
         server.getPluginManager().clearEvents();
 
         // Assert the talisman activates in the ender chest
-        activateAnvilTalisman(true);
+        activateAnvilTalisman(true, true);
         server.getPluginManager().assertEventFired(TalismanActivateEvent.class, event -> {
             Assertions.assertTrue(event.isCancelled());
             return true;
