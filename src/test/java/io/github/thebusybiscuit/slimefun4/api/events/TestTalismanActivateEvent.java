c28244cc18983a00dcfa95a58e79ea07ce1429e4
Alessio Colombo <37039432+Sfiguz7@users.noreply.github.com>
2023-12-12 21:27:37 +0100

    Added negation tests

--- src/test/java/io/github/thebusybiscuit/slimefun4/api/events/TestTalismanActivateEvent.java
+++ src/test/java/io/github/thebusybiscuit/slimefun4/api/events/TestTalismanActivateEvent.java
@@ -78,6 +78,23 @@ class TestTalismanActivateEvent {
         activateAnvilTalisman(true);
         server.getPluginManager().assertEventFired(TalismanActivateEvent.class, ignored -> true);
         server.getPluginManager().clearEvents();
+        // Assert the normal talisman does not activate in the ender chest
+        activateAnvilTalisman(false, true);
+        try {
+            server.getPluginManager().assertEventFired(TalismanActivateEvent.class, ignored -> true);
+        } catch (AssertionError ignored) {
+            return; // This is expected; the event should not have fired
+        }
+        server.getPluginManager().clearEvents();
+
+        // Assert the ender talisman does not activate in the inventory
+        try {
+            activateAnvilTalisman(true, false);
+            server.getPluginManager().assertEventFired(TalismanActivateEvent.class, ignored -> true);
+        } catch (AssertionError ignored) {
+            return; // This is expected; the event should not have fired
+        }
+        server.getPluginManager().clearEvents();
     }
 
     @Test
