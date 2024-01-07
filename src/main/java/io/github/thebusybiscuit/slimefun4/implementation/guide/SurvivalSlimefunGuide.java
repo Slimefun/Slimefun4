38967df9d64f3f780654f09bf117e4a34e522280
ybw0014 <i@guizhanss.com>
2024-01-07 13:17:21 -0500

    refactor: update the button, make the name shorter

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/guide/SurvivalSlimefunGuide.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/guide/SurvivalSlimefunGuide.java
@@ -514,12 +514,19 @@ public class SurvivalSlimefunGuide implements SlimefunGuideImplementation {
         Optional<String> wiki = item.getWikipage();
 
         if (wiki.isPresent()) {
-            String message = Slimefun.getLocalization().getMessage(p, "guide.tooltips.wiki-third-party")
+            String message = Slimefun.getLocalization().getMessage(p, "guide.tooltips.wiki.third-party")
                 .replace("%addon%", item.getAddon().getName());
             if (item.getAddon() instanceof Slimefun) {
-                message = Slimefun.getLocalization().getMessage(p, "guide.tooltips.wiki");
+                message = Slimefun.getLocalization().getMessage(p, "guide.tooltips.wiki.slimefun");
             }
-            menu.addItem(8, new CustomItemStack(Material.KNOWLEDGE_BOOK, ChatColor.WHITE + message, "", ChatColor.GRAY + "\u21E8 " + ChatColor.GREEN + Slimefun.getLocalization().getMessage(p, "guide.tooltips.open-itemgroup")));
+            menu.addItem(8, new CustomItemStack(
+                Material.KNOWLEDGE_BOOK,
+                ChatColor.WHITE + Slimefun.getLocalization().getMessage(p, "guide.tooltips.wiki.button"),
+                "",
+                ChatColor.WHITE + message,
+                "",
+                ChatColor.GRAY + "\u21E8 " + ChatColor.GREEN + Slimefun.getLocalization().getMessage(p, "guide.tooltips.open-itemgroup")
+            ));
             menu.addMenuClickHandler(8, (pl, slot, itemstack, action) -> {
                 pl.closeInventory();
                 if (!(item.getAddon() instanceof Slimefun)) {
