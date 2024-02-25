b841828454168bbdbb211a9aa41d9ab244389c60
Daniel Walsh <walshydev@gmail.com>
2024-02-25 18:24:21 +0000

    Remove Unirest and replace with Java's native HttpClient (#4068)
    Co-authored-by: J3fftw <44972470+J3fftw1@users.noreply.github.com>

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/services/github/ContributionsConnector.java
@@ -9,11 +9,11 @@ import java.util.logging.Level;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
-import kong.unirest.JsonNode;
-import kong.unirest.json.JSONArray;
-import kong.unirest.json.JSONObject;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 
 class ContributionsConnector extends GitHubConnector {
 
@@ -90,11 +90,11 @@ class ContributionsConnector extends GitHubConnector {
     }
 
     @Override
-    public void onSuccess(@Nonnull JsonNode response) {
+    public void onSuccess(@Nonnull JsonElement response) {
         finished = true;
 
-        if (response.isArray()) {
-            computeContributors(response.getArray());
+        if (response.isJsonArray()) {
+            computeContributors(response.getAsJsonArray());
         } else {
             Slimefun.logger().log(Level.WARNING, "Received an unusual answer from GitHub, possibly a timeout? ({0})", response);
         }
@@ -123,13 +123,13 @@ class ContributionsConnector extends GitHubConnector {
         return parameters;
     }
 
-    private void computeContributors(@Nonnull JSONArray array) {
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject object = array.getJSONObject(i);
+    private void computeContributors(@Nonnull JsonArray array) {
+        for (JsonElement element : array) {
+            JsonObject object = element.getAsJsonObject();
 
-            String name = object.getString("login");
-            int commits = object.getInt("contributions");
-            String profile = object.getString("html_url");
+            String name = object.get("login").getAsString();
+            int commits = object.get("contributions").getAsInt();
+            String profile = object.get("html_url").getAsString();
 
             if (!ignoredAccounts.contains(name)) {
                 String username = aliases.getOrDefault(name, name);
