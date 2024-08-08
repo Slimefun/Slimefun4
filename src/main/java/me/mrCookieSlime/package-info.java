5b8d872296a17b124fa247a2551187b4e422ca8a
Seggan <seggan21@gmail.com>
2024-08-08 12:24:12 -0400

    [ci skip] Undeprecate the CsCore stuff for now (#4187)

--- src/main/java/me/mrCookieSlime/package-info.java
+++ src/main/java/me/mrCookieSlime/package-info.java
@@ -3,5 +3,4 @@
  * Don't look too close at the code that lays here. It's horrible, believe me.
  * Once we updated everything, all of these classes will be removed.
  */
-@java.lang.Deprecated
 package me.mrCookieSlime;
