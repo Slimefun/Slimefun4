5bcd79da495b2445b0f843867bc126d68d267e99
Intybyte <vaan.testwork@gmail.com>
2024-08-31 09:34:52 +0200

    Add documentation

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/ElectricJuicer.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/electric/machines/ElectricJuicer.java
@@ -4,12 +4,20 @@ import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
+import io.github.thebusybiscuit.slimefun4.implementation.items.multiblocks.Juicer;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 
+/**
+ * The {@link ElectricJuicer} is an electric machine based on the {@link Juicer}.
+ *
+ * @author Vaan1310
+ *
+ * @see Juicer
+ */
 public class ElectricJuicer extends AContainer implements RecipeDisplayItem {
     public ElectricJuicer(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
