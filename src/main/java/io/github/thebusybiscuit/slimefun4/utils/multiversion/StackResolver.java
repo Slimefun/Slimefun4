b75158a23e450b1f0b3010fea7ecd257087a5b0f
Intybyte <vaan.testwork@gmail.com>
2024-09-25 21:01:38 +0200

    Add StackResolver for backwards compatibility

---
+++ src/main/java/io/github/thebusybiscuit/slimefun4/utils/multiversion/StackResolver.java
@@ -0,0 +1,31 @@
+package io.github.thebusybiscuit.slimefun4.utils.multiversion;
+
+import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * This utility class provides backwards compatibility to versions of minecraft
+ * regarding the creation of ItemStacks.
+ *
+ * @author Vaan1310
+ */
+public final class StackResolver {
+
+    private StackResolver() {}
+
+    public static ItemStack of(Material material) {
+        return of(material, 1);
+    }
+
+    public static ItemStack of(Material material, int amount) {
+        var version = Slimefun.getMinecraftVersion();
+
+        if (version.isAtLeast(MinecraftVersion.MINECRAFT_1_21)) {
+            return ItemStack.of(material, amount);
+        } else {
+            return new ItemStack(material, amount);
+        }
+    }
+}
