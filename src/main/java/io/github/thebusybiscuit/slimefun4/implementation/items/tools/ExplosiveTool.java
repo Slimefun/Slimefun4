c84e87627dd76e376b72c0e658572f7546f50517
Daniel Walsh <walshydev@gmail.com>
2024-10-01 02:17:35 +0100

    Update to 1.21

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/tools/ExplosiveTool.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/items/tools/ExplosiveTool.java
@@ -1,7 +1,9 @@
 package io.github.thebusybiscuit.slimefun4.implementation.items.tools;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -9,6 +11,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import dev.lone.itemsadder.api.CustomBlock;
 import org.bukkit.Bukkit;
 import org.bukkit.Effect;
+import org.bukkit.ExplosionResult;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -17,6 +20,7 @@ import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.inventory.ItemStack;
 
 import io.github.bakedlibs.dough.protection.Interaction;
+import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
 import io.github.thebusybiscuit.slimefun4.api.events.ExplosiveToolBreakBlocksEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting;
@@ -48,6 +52,17 @@ public class ExplosiveTool extends SimpleSlimefunItem<ToolUseHandler> implements
     private final ItemSetting<Boolean> damageOnUse = new ItemSetting<>(this, "damage-on-use", true);
     private final ItemSetting<Boolean> callExplosionEvent = new ItemSetting<>(this, "call-explosion-event", false);
 
+    private static Constructor<?> pre21ExplodeEventConstructor;
+    static {
+        if (Slimefun.getMinecraftVersion().isBefore(MinecraftVersion.MINECRAFT_1_21)) {
+            try {
+                pre21ExplodeEventConstructor = BlockExplodeEvent.class.getConstructor(Block.class, List.class, float.class);
+            } catch (Exception e) {
+                Slimefun.logger().log(Level.SEVERE, "Could not find constructor for BlockExplodeEvent", e);
+            }
+        }
+    }
+
     @ParametersAreNonnullByDefault
     public ExplosiveTool(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
@@ -78,7 +93,7 @@ public class ExplosiveTool extends SimpleSlimefunItem<ToolUseHandler> implements
         List<Block> blocksToDestroy = new ArrayList<>();
 
         if (callExplosionEvent.getValue()) {
-            BlockExplodeEvent blockExplodeEvent = new BlockExplodeEvent(b, blocks, 0);
+            BlockExplodeEvent blockExplodeEvent = createNewBlockExplodeEvent(b, blocks, 0);
             Bukkit.getServer().getPluginManager().callEvent(blockExplodeEvent);
 
             if (!blockExplodeEvent.isCancelled()) {
@@ -186,4 +201,24 @@ public class ExplosiveTool extends SimpleSlimefunItem<ToolUseHandler> implements
         damageItem(p, item);
     }
 
+    private BlockExplodeEvent createNewBlockExplodeEvent(
+        Block block,
+        List<Block> blocks,
+        float yield
+    ) {
+        var version = Slimefun.getMinecraftVersion();
+        if (version.isAtLeast(MinecraftVersion.MINECRAFT_1_21)) {
+            return new BlockExplodeEvent(block, block.getState(), blocks, yield, ExplosionResult.DESTROY);
+        } else if (pre21ExplodeEventConstructor != null) {
+            try {
+                return (BlockExplodeEvent) pre21ExplodeEventConstructor.newInstance(block, blocks, yield);
+            } catch (Exception e) {
+                Slimefun.logger().log(Level.SEVERE, "Could not find constructor for BlockExplodeEvent", e);
+            }
+
+            return null;
+        } else {
+            throw new IllegalStateException("BlockExplodeEvent constructor not found");
+        }
+    }
 }
