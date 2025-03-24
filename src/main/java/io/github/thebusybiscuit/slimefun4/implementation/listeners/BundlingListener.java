42655d163cde8a2f9c7f5441b53ca357924305fa
Intybyte <vaan.testwork@gmail.com>
2025-03-24 15:09:17 +0100

    Fix BundlingListener

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/BundlingListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/BundlingListener.java
@@ -1,9 +1,13 @@
 package io.github.thebusybiscuit.slimefun4.implementation.listeners;
 
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.implementation.items.backpacks.SlimefunBackpack;
 import io.github.thebusybiscuit.slimefun4.utils.tags.SlimefunTag;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -13,15 +17,34 @@ public class BundlingListener implements Listener {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    public void onBundling(InventoryDragEvent event) {
-
+    @EventHandler(ignoreCancelled = true)
+    public void onBundling(InventoryClickEvent event) {
         ItemStack cursor = event.getCursor();
-        if (cursor == null) {
+        ItemStack slot = event.getCurrentItem();
+
+        if (slot == null) {
             return;
         }
 
-        if (!SlimefunTag.BUNDLES.isTagged(cursor.getType())) {
-            return;
+        if (isBundle(cursor) && isBackpack(slot) || isBundle(slot) && isBackpack(cursor)) {
+            event.setCancelled(true);
         }
+    }
+
+    private boolean isBundle(ItemStack stack) {
+        return SlimefunTag.BUNDLES.isTagged(stack.getType());
+    }
+
+    private boolean isBackpack(ItemStack stack) {
+        if (stack.getType() != Material.PLAYER_HEAD) {
+            return false;
+        }
+
+        SlimefunItem backpack = SlimefunItem.getByItem(stack);
+        if (backpack == null) {
+            return false;
+        }
+
+        return backpack instanceof SlimefunBackpack;
     }
 }
