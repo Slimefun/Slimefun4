343ffd5ca570de6c7c603d3a270c836470918ace
intisy (Finn Birich) <finn@birich.de>
2026-06-13 22:41:51 +0200

    chore(deps): bump dough to 4.0.4 for version-safe PersistentDataAPI

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -47,7 +47,7 @@ repositories {
 dependencies {
     implementation(project(":compat-api"))
 
-    githubImplementation("Slimefun5:dough:4.0.2:all")
+    githubImplementation("Slimefun5:dough:4.0.4:all")
 
     implementation("io.papermc:paperlib:1.0.8")
     implementation("commons-lang:commons-lang:2.6")
